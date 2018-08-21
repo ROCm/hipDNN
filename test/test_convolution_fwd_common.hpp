3acbe0b6a6267bdf31f38cad95ee77e07f6f7e10
Neelakandan <neelakandan@multicorewareinc.com>
2018-08-21 13:03:54 +0530

    Add structures and headers to bookkeep convolution descriptions
    [ROCm/hipDNN commit: c83ec5c3e28f244a3178aa38cd513c1db93d2db1]

---
+++ test/test_convolution_fwd_common.hpp
@@ -0,0 +1,10 @@
+#ifndef TEST_CONVOLUTION_FORWARD_COMMON_HPP
+#define TEST_CONVOLUTION_FORWARD_COMMON_HPP
+
+#include "gtest/gtest.h"
+
+void compute_ref_conv_fwd() {
+
+}
+
+#endif //TEST_CONVOLUTION_FORWARD_COMMON_HPP
