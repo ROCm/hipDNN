0728104b0f5829b4eabb9d1bbd8b689d958c4029
Neelakandan <neelakandan@multicorewareinc.com>
2018-08-23 13:34:55 +0530

    Add utility functions to book keep size, memoryInfo and populateRandom the buffer

--- test/test_convolution_fwd.cpp
+++ test/test_convolution_fwd.cpp
@@ -2,5 +2,15 @@
 #include "test_convolution_fwd_common.hpp"
 
 TEST(convolution_fwd, func_check_zero_padding) {
+    Desc inputDesc(3, 3, 224, 224);
+    Desc filterDesc(21, 3, 3, 3);
+    int pad[2]={0, 0}; // zero padding
+    int stride[2]={1, 1};  // stride 1
+    Desc outputDesc = calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
+    Memory<float> srcData = createMemory<float>(inputDesc);
+    Memory<float> dstData = createMemory<float>(outputDesc);
+    Memory<float> filterData = createMemory<float>(filterDesc);
+    populateMemoryRandom<float>(srcData);
+    populateMemoryRandom<float>(filterData);
 	EXPECT_EQ(0, 0);
 }
