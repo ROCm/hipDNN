3015a8fa62f71759ea178f40aeeef53f342150ca
neel <neelakandan@multicorewareinc.com>
2018-08-23 15:33:03 +0530

    Add Equals module to check and compare conv_fwd of cpu and gpu

--- test/test_convolution_fwd.cpp
+++ test/test_convolution_fwd.cpp
@@ -8,9 +8,17 @@ TEST(convolution_fwd, func_check_zero_padding) {
     int stride[2]={1, 1};  // stride 1
     Desc outputDesc = calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
     Memory<float> srcData = createMemory<float>(inputDesc);
-    Memory<float> dstData = createMemory<float>(outputDesc);
+    Memory<float> dstDataCPU = createMemory<float>(outputDesc);
+    Memory<float> dstDataGPU = createMemory<float>(outputDesc);
     Memory<float> filterData = createMemory<float>(filterDesc);
     populateMemoryRandom<float>(srcData);
     populateMemoryRandom<float>(filterData);
+    test_convolution_sizes_t testConvolutionSizes(inputDesc.N, 1, inputDesc.C,
+            inputDesc.H, inputDesc.W, outputDesc.C, outputDesc.H, outputDesc.W,
+            filterDesc.H, filterDesc.W, pad[0], pad[1], stride[0], stride[1], 1, 1);
+
+    compute_cpuref_conv_fwd<float>(testConvolutionSizes, srcData.cpu(), filterData.cpu(), NULL, dstDataCPU.cpu());
+    compute_hipdnn_conv_fwd<float>(testConvolutionSizes, srcData.gpu(), filterData.gpu(), NULL, dstDataGPU.gpu());
+    Equals<float>(dstDataCPU, dstDataGPU);
 	EXPECT_EQ(0, 0);
 }
