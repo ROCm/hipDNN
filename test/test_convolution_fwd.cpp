0a1149a04e035eff8b25c972e9a83a10d82a952b
Neelakandan <neelakandan@multicorewareinc.com>
2018-08-23 15:59:41 +0530

    Begin with small input convolutions
    [ROCm/hipDNN commit: ced128950839ccd43e2d85e8b2cfcf5da19f09fb]

--- test/test_convolution_fwd.cpp
+++ test/test_convolution_fwd.cpp
@@ -2,8 +2,8 @@
 #include "test_convolution_fwd_common.hpp"
 
 TEST(convolution_fwd, func_check_zero_padding) {
-    Desc inputDesc(3, 3, 224, 224);
-    Desc filterDesc(21, 3, 3, 3);
+    Desc inputDesc(1, 3, 5, 5);
+    Desc filterDesc(1, 3, 3, 3);
     int pad[2]={0, 0}; // zero padding
     int stride[2]={1, 1};  // stride 1
     Desc outputDesc = calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
