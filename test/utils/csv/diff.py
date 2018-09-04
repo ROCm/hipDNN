0fb7b248c19aea9d468f54e57e1d326a014a357a
mcw <neelakandan@multicorewareinc.com>
2018-09-04 12:09:44 +0530

    Added csv utils files
    [ROCm/hipDNN commit: cc72e152e38f310576ecd9d6956773a4e43b31a4]

---
+++ test/utils/csv/diff.py
@@ -0,0 +1,29 @@
+import csv
+import argparse
+
+reader1 = csv.DictReader(open('new_amd_result.csv', 'r'))
+amd_op = []
+for line1 in reader1:
+	amd_op.append(line1)
+	
+reader2 = csv.DictReader(open('new_nvidia_result.csv', 'r'))
+nv_op = []
+
+for line2 in reader2:
+	nv_op.append(line2)
+	
+def test_compare(amd_op,nv_op):
+	for (line1,line2) in zip(amd_op,nv_op):
+		if (line1['Test_name'] == line2['Test_name']):
+			if (line1['Output'] == line2['Output']):
+				print ("test pass for "+line1['Test_name'])
+				print(sum(c1!=c2 for c1,c2 in zip(line1['Output'],line2['Output'])))
+				
+			else:
+				print ("test fail for "+line1['Test_name']+"!!!difference in outputs!!!!")
+				print(sum(c1!=c2 for c1,c2 in zip(line1['Output'],line2['Output'])))
+						
+		else:
+			print ("error in test cases!!!")
+			
+test_compare(amd_op,nv_op)
