caf7f1226077e426abdf128b9b5adf1edc26e5d3
mcw <neelakandan@multicorewareinc.com>
2018-09-05 18:34:19 +0530

    Added activation forward test
    [ROCm/hipDNN commit: 8d3e3b3b12df1cbc9753f4d1bf3535c3d065a83a]

---
+++ test/test_activation_backward.cpp
@@ -0,0 +1,11 @@
+#include "test_activation_backward.hpp"
+
+TEST(activation_backward, func_test_naive_activation){
+    activation_params_t test_case(1, 1, 224, 244);
+    Memory<float> dataSrc(test_case.n * test_case.channels * test_case.height * test_case.width);
+    Memory<float> dataGrad(test_case.n * test_case.channels * test_case.height * test_case.width);
+    Memory<float> dataDst(test_case.n * test_case.channels * test_case.height * test_case.width);
+    populateMemoryRandom(dataSrc);
+    populateMemoryRandom(dataGrad);
+    compute_hipdnn_activation_backward(test_case, dataSrc.gpu(), dataGrad.gpu(), dataDst.gpu());
+}
---
+++ test/test_activation_backward.hpp
@@ -0,0 +1,45 @@
+  #include "hipDNN_test_common.h"
+  
+struct activation_params_t{
+      int n, channels, height, width;
+      activation_params_t(int n, int channels, int height, int width): n(n), channels(channels), height(height), width(width){}
+};
+
+template<typename dataType>
+void compute_hipdnn_activation_backward(activation_params_t& test_case, dataType* src, dataType* grad, dataType* dst){
+  hipdnnHandle_t hipdnn;
+  checkHIPDNN(hipdnnCreate(&hipdnn));
+  hipdnnTensorDescriptor_t in_desc;
+  checkHIPDNN(hipdnnCreateTensorDescriptor(&in_desc));
+  checkHIPDNN(hipdnnSetTensor4dDescriptor(
+        in_desc, HIPDNN_TENSOR_NCHW, HIPDNN_DATA_FLOAT,
+        test_case.n, test_case.channels, test_case.height, test_case.width));
+      hipdnnActivationDescriptor_t activationDesc;
+    hipdnnActivationMode_t mode = HIPDNN_ACTIVATION_SIGMOID;
+  hipdnnNanPropagation_t reluNanOpt = HIPDNN_NOT_PROPAGATE_NAN;
+  double reluCeilingOrAlpha=0;
+  double activBeta=0;
+  double activExp=0;
+  checkHIPDNN(hipdnnCreateActivationDescriptor(&activationDesc));
+  checkHIPDNN(hipdnnSetActivationDescriptor(activationDesc, mode,
+            reluNanOpt,reluCeilingOrAlpha, activBeta, activExp));
+     
+  hipdnnTensorDescriptor_t out_desc;
+  checkHIPDNN(hipdnnCreateTensorDescriptor(&out_desc));
+  checkHIPDNN(hipdnnSetTensor4dDescriptor(
+        out_desc, HIPDNN_TENSOR_NCHW, HIPDNN_DATA_FLOAT,
+        test_case.n, test_case.channels, test_case.height, test_case.width));
+  float alpha = 1.f;
+  float beta = 0.f;
+
+  hipdnnActivationForward(hipdnn, activationDesc, &alpha, in_desc, src,
+                          &beta, out_desc, dst);
+ 
+  hipdnnActivationBackward( hipdnn, activationDesc, &alpha, in_desc, src,
+              in_desc, src, out_desc, dst, &beta, out_desc, grad);
+  
+  hipdnnDestroyTensorDescriptor(out_desc);
+  hipdnnDestroyActivationDescriptor(activationDesc);
+  hipdnnDestroyTensorDescriptor(in_desc);
+  hipdnnDestroy(hipdnn);
+}
