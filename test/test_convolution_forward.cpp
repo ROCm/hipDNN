0a835dd963cf65049f04fda3b06eeaf150688ebc
mcw <neelakandan@multicorewareinc.com>
2018-09-06 12:25:17 +0530

    Added csv dump support
    [ROCm/hipDNN commit: e7ff805e00b41b2283d40fb410874eddf2e2b518]

--- test/test_convolution_forward.cpp
+++ test/test_convolution_forward.cpp
@@ -1,89 +1,139 @@
 #include "test_convolution_forward.hpp"
-#include "utils/csv/csv_integration.hpp"
 
 TEST(convolution_fwd, func_check_zero_padding) {
-  Desc inputDesc(1, 3, 224, 224);
-  Desc filterDesc(21, 3, 3, 3);
-  int pad[2] = {0, 0};    // zero padding
-  int stride[2] = {1, 1}; // stride 1
-  Desc outputDesc =
-      calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
-  Memory<float> srcData = createMemory<float>(inputDesc);
-  Memory<float> dstDataCPU = createMemory<float>(outputDesc);
-  Memory<float> dstDataGPU = createMemory<float>(outputDesc);
-  Memory<float> filterData = createMemory<float>(filterDesc);
-  populateMemoryRandom<float>(srcData);
-  populateMemoryRandom<float>(filterData);
-  test_convolution_sizes_t testConvolutionSizes(
-      inputDesc.N, 1, inputDesc.C, inputDesc.H, inputDesc.W, outputDesc.C,
-      outputDesc.H, outputDesc.W, filterDesc.H, filterDesc.W, pad[0], pad[1],
-      stride[0], stride[1], 1, 1);
+    Desc inputDesc(1, 3, 224, 224);
+    Desc filterDesc(21, 3, 3, 3);
+    int pad[2] = {0, 0};    // zero padding
+    int stride[2] = {1, 1}; // stride 1
+    Desc outputDesc =
+        calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
+    Memory<float> srcData = createMemory<float>(inputDesc);
+    Memory<float> dstDataCPU = createMemory<float>(outputDesc);
+    Memory<float> dstDataGPU = createMemory<float>(outputDesc);
+    Memory<float> filterData = createMemory<float>(filterDesc);
+    populateMemoryRandom<float>(srcData);
+    populateMemoryRandom<float>(filterData);
+    test_convolution_sizes_t testConvolutionSizes(
+        inputDesc.N, 1, inputDesc.C, inputDesc.H, inputDesc.W, outputDesc.C,
+        outputDesc.H, outputDesc.W, filterDesc.H, filterDesc.W, pad[0], pad[1],
+        stride[0], stride[1], 1, 1);
 
-  compute_hipdnn_conv_fwd<float>(testConvolutionSizes, srcData.gpu(),
-                                 filterData.gpu(), NULL, dstDataGPU.gpu());
+    compute_hipdnn_conv_fwd<float>(testConvolutionSizes, srcData.gpu(),
+                                   filterData.gpu(), NULL, dstDataGPU.gpu());
 
-  std::string strt = "./result_unittest.csv";
-  std::string testname = "func_check_zero_padding";
-  float *temp = dstDataGPU.getDataFromGPU();
-  std::string str =
-      convert_to_string((float *)temp, (int)dstDataGPU.get_num_elements());
-  write_to_csv(strt, str, testname);
+    std::string strt = "./result_unittest.csv";
+    std::string testname = "func_check_zero_padding";
+    float* temp = dstDataGPU.getDataFromGPU();
+    std::string str  = convert_to_string((float*)temp,(int)dstDataGPU.get_num_elements());
+    write_to_csv(strt, str, testname);
 }
 
-TEST(convolution_fwd, func_check_two_strides) {
-  Desc inputDesc(1, 3, 224, 224);
-  Desc filterDesc(21, 3, 3, 3);
-  int pad[2] = {0, 0};    // zero padding
-  int stride[2] = {2, 2}; // stride 1
-  Desc outputDesc =
-      calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
-  Memory<float> srcData = createMemory<float>(inputDesc);
-  Memory<float> dstDataCPU = createMemory<float>(outputDesc);
-  Memory<float> dstDataGPU = createMemory<float>(outputDesc);
-  Memory<float> filterData = createMemory<float>(filterDesc);
-  populateMemoryRandom<float>(srcData);
-  populateMemoryRandom<float>(filterData);
-  test_convolution_sizes_t testConvolutionSizes(
-      inputDesc.N, 1, inputDesc.C, inputDesc.H, inputDesc.W, outputDesc.C,
-      outputDesc.H, outputDesc.W, filterDesc.H, filterDesc.W, pad[0], pad[1],
-      stride[0], stride[1], 1, 1);
+TEST(convolution_fwd, func_check_two_strides_medium_kernelsize) {
+    Desc inputDesc(1, 3, 600, 600);
+    Desc filterDesc(2, 3, 30, 30);
+    int pad[2] = {0, 0};    // zero padding
+    int stride[2] = {2, 2}; // stride 1
+    Desc outputDesc =
+        calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
+    Memory<float> srcData = createMemory<float>(inputDesc);
+    Memory<float> dstDataCPU = createMemory<float>(outputDesc);
+    Memory<float> dstDataGPU = createMemory<float>(outputDesc);
+    Memory<float> filterData = createMemory<float>(filterDesc);
+    populateMemoryRandom<float>(srcData);
+    populateMemoryRandom<float>(filterData);
+    test_convolution_sizes_t testConvolutionSizes(
+        inputDesc.N, 1, inputDesc.C, inputDesc.H, inputDesc.W, outputDesc.C,
+        outputDesc.H, outputDesc.W, filterDesc.H, filterDesc.W, pad[0], pad[1],
+        stride[0], stride[1], 1, 1);
 
-  compute_hipdnn_conv_fwd<float>(testConvolutionSizes, srcData.gpu(),
-                                 filterData.gpu(), NULL, dstDataGPU.gpu());
+    compute_hipdnn_conv_fwd<float>(testConvolutionSizes, srcData.gpu(),
+                                   filterData.gpu(), NULL, dstDataGPU.gpu());
 
-  std::string strt = "./result_unittest.csv";
-  std::string testname = "func_check_two_strides";
-  float *temp = dstDataGPU.getDataFromGPU();
-  std::string str =
-      convert_to_string((float *)temp, (int)dstDataGPU.get_num_elements());
-  write_to_csv(strt, str, testname);
+    std::string strt = "./result_unittest.csv";
+    std::string testname = "func_check_two_strides_medium_kernelsize";
+    float* temp = dstDataGPU.getDataFromGPU();
+    std::string str  = convert_to_string((float*)temp,(int)dstDataGPU.get_num_elements());
+    write_to_csv(strt, str, testname);
 }
 
-TEST(convolution_fwd, func_check_padding_and_strides) {
-  Desc inputDesc(1, 3, 224, 224);
-  Desc filterDesc(21, 3, 3, 3);
-  int pad[2] = {2, 2};    // zero padding
-  int stride[2] = {2, 2}; // stride 1
-  Desc outputDesc =
-      calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
-  Memory<float> srcData = createMemory<float>(inputDesc);
-  Memory<float> dstDataCPU = createMemory<float>(outputDesc);
-  Memory<float> dstDataGPU = createMemory<float>(outputDesc);
-  Memory<float> filterData = createMemory<float>(filterDesc);
-  populateMemoryRandom<float>(srcData);
-  populateMemoryRandom<float>(filterData);
-  test_convolution_sizes_t testConvolutionSizes(
-      inputDesc.N, 1, inputDesc.C, inputDesc.H, inputDesc.W, outputDesc.C,
-      outputDesc.H, outputDesc.W, filterDesc.H, filterDesc.W, pad[0], pad[1],
-      stride[0], stride[1], 1, 1);
+TEST(convolution_fwd, func_check_padding_and_strides_small_size) {
+    Desc inputDesc(1, 3, 7, 7);
+    Desc filterDesc(21, 3, 3, 3);
+    int pad[2] = {2, 2};    // zero padding
+    int stride[2] = {2, 2}; // stride 1
+    Desc outputDesc =
+        calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
+    Memory<float> srcData = createMemory<float>(inputDesc);
+    Memory<float> dstDataCPU = createMemory<float>(outputDesc);
+    Memory<float> dstDataGPU = createMemory<float>(outputDesc);
+    Memory<float> filterData = createMemory<float>(filterDesc);
+    populateMemoryRandom<float>(srcData);
+    populateMemoryRandom<float>(filterData);
+    test_convolution_sizes_t testConvolutionSizes(
+        inputDesc.N, 1, inputDesc.C, inputDesc.H, inputDesc.W, outputDesc.C,
+        outputDesc.H, outputDesc.W, filterDesc.H, filterDesc.W, pad[0], pad[1],
+        stride[0], stride[1], 1, 1);
 
-  compute_hipdnn_conv_fwd<float>(testConvolutionSizes, srcData.gpu(),
-                                 filterData.gpu(), NULL, dstDataGPU.gpu());
+    compute_hipdnn_conv_fwd<float>(testConvolutionSizes, srcData.gpu(),
+                                   filterData.gpu(), NULL, dstDataGPU.gpu());
 
-  std::string strt = "./result_unittest.csv";
-  std::string testname = "func_check_padding_and_stride";
-  float *temp = dstDataGPU.getDataFromGPU();
-  std::string str =
-      convert_to_string((float *)temp, (int)dstDataGPU.get_num_elements());
-  write_to_csv(strt, str, testname);
+    std::string strt = "./result_unittest.csv";
+    std::string testname = "func_check_padding_and_strides_small_size";
+    float* temp = dstDataGPU.getDataFromGPU();
+    std::string str  = convert_to_string((float*)temp,(int)dstDataGPU.get_num_elements());
+    write_to_csv(strt, str, testname);
+}
+
+TEST(convolution_fwd, func_check_full_conv) {
+    Desc inputDesc(1, 3, 7, 7);
+    Desc filterDesc(3, 3, 7, 7);
+    int pad[2] = {0, 0};    // zero padding
+    int stride[2] = {1, 1}; // stride 1
+    Desc outputDesc =
+        calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
+    Memory<float> srcData = createMemory<float>(inputDesc);
+    Memory<float> dstDataCPU = createMemory<float>(outputDesc);
+    Memory<float> dstDataGPU = createMemory<float>(outputDesc);
+    Memory<float> filterData = createMemory<float>(filterDesc);
+    populateMemoryRandom<float>(srcData);
+    populateMemoryRandom<float>(filterData);
+    test_convolution_sizes_t testConvolutionSizes(
+        inputDesc.N, 1, inputDesc.C, inputDesc.H, inputDesc.W, outputDesc.C,
+        outputDesc.H, outputDesc.W, filterDesc.H, filterDesc.W, pad[0], pad[1],
+        stride[0], stride[1], 1, 1);
+
+    compute_hipdnn_conv_fwd<float>(testConvolutionSizes, srcData.gpu(),
+                                   filterData.gpu(), NULL, dstDataGPU.gpu());
+
+    std::string strt = "./result_unittest.csv";
+    std::string testname = "func_check_full_conv";
+    float* temp = dstDataGPU.getDataFromGPU();
+    std::string str  = convert_to_string((float*)temp,(int)dstDataGPU.get_num_elements());
+    write_to_csv(strt, str, testname);
+}
+
+TEST(convolution_fwd, func_check_dilation1x1) {
+Desc inputDesc(1, 3, 7, 7);
+Desc filterDesc(3, 3, 3, 3);
+int pad[2] = {2, 2}; // zero padding
+int stride[2] = {1, 1}; // stride 1
+Desc outputDesc =
+calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
+Memory<float> srcData = createMemory<float>(inputDesc);
+Memory<float> dstDataCPU = createMemory<float>(outputDesc);
+Memory<float> dstDataGPU = createMemory<float>(outputDesc);
+Memory<float> filterData = createMemory<float>(filterDesc);
+populateMemoryRandom<float>(srcData);
+populateMemoryRandom<float>(filterData);
+test_convolution_sizes_t testConvolutionSizes(
+inputDesc.N, 1, inputDesc.C, inputDesc.H, inputDesc.W, outputDesc.C,
+outputDesc.H, outputDesc.W, filterDesc.H, filterDesc.W, pad[0], pad[1],
+stride[0], stride[1], 1, 1);
+compute_hipdnn_conv_fwd<float>(testConvolutionSizes, srcData.gpu(),
+filterData.gpu(), NULL, dstDataGPU.gpu());
+std::string strt = "./result_unittest.csv";
+std::string testname = "func_check_dilation1x1";
+float* temp = dstDataGPU.getDataFromGPU();
+std::string str = convert_to_string((float*)temp,(int)dstDataGPU.get_num_elements());
+write_to_csv(strt, str, testname);
 }
