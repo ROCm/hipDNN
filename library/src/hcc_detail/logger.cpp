ab7c5d7c53485de338ce2df80dafe1f1bc34f30d
mcw <neelakandan@multicorewareinc.com>
2018-09-06 10:35:02 +0530

    clang-formatted the files

--- library/src/hcc_detail/logger.cpp
+++ library/src/hcc_detail/logger.cpp
@@ -4,7 +4,7 @@
 namespace open {
 
 int IsLogging(const LoggingLevel level) {
-    const int enable_level = DEBUG_CURRENT_CALL_STACK_LEVEL;
-    return enable_level >= ((int) level - 1);
+  const int enable_level = DEBUG_CURRENT_CALL_STACK_LEVEL;
+  return enable_level >= ((int)level - 1);
 }
-}
+} // namespace open
