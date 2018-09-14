969ea2d22f25567c3a342950ef57a19dc683ede8
pradeepisro <pradeepisro49@gmail.com>
2018-09-14 10:58:36 +0530

    temp
    [ROCm/hipDNN commit: 3d714865e70fb0fd849b05ea49d949f86dc819f5]

---
+++ test/test_convolution_backward_img.cpp
@@ -0,0 +1,33 @@
+#include "test_convolution_backward_img.hpp"
+
+TEST(convolution_bwd_img, func_check_backward_conv_img) {
+    Desc inputDesc(1, 3, 224, 224);
+    Desc filterDesc(21, 3, 3, 3);
+    int pad[2] = {0, 0};    // zero padding
+    int stride[2] = {1, 1}; // stride 1
+    Desc outputDesc =
+        calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
+    Memory<float> srcData = createMemory<float>(inputDesc);
+    Memory<float> dstDataCPU = createMemory<float>(outputDesc);
+    Memory<float> dstDataGPU = createMemory<float>(outputDesc);
+    Memory<float> filterData = createMemory<float>(filterDesc);
+    populateMemoryRandom<float>(srcData);
+    populateMemoryRandom<float>(filterData);
+    test_convolution_sizes_t testConvolutionSizes(
+        inputDesc.N, 1, inputDesc.C, inputDesc.H, inputDesc.W, outputDesc.C,
+        outputDesc.H, outputDesc.W, filterDesc.H, filterDesc.W, pad[0], pad[1],
+        stride[0], stride[1], 1, 1);
+high_resolution_timer_t timer;
+    compute_hipdnn_conv_fwd<float>(testConvolutionSizes, srcData.gpu(),
+                                   filterData.gpu(), NULL, dstDataGPU.gpu());
+  std::uint64_t time_elapsed = timer.elapsed_nanoseconds();
+  std::cout << "time taken: " << (time_elapsed / 1000.0) << " ms"<< std::endl;
+
+    std::string strt = "./result_unittest.csv";
+    std::string testname = "func_check_backward_conv_img";
+    float* temp = dstDataGPU.getDataFromGPU();
+    std::string str  = convert_to_string((float*)temp,(int)dstDataGPU.get_num_elements());
+    write_to_csv(strt, str, testname);
+}
+
+
--- test/test_pooling_common.hpp
+++ test/test_pooling_common.hpp
@@ -1,22 +1,37 @@
-#ifndef POOLING_BACKWARD
-#define POOLING_BACKWARD
+#ifndef TEST_POOLING_COMMON_H
+#define TEST_POOLING_COMMON_H
+
+#include "hipDNN.h"
 #include "hipDNN_test_common.h"
 
-struct test_pooling_t {
+struct pool_fwd {
+  int mb, c;      // Minibatch and channels
+  int ih, iw;     // input dimensions
+  int oh, ow;     // output dimensions
+  int kh, kw;     // kernel dimensions
+  int padt, padl; // padding dimensions
+  int strh, strw; // stride dimensions
+  pool_fwd(int mb, int c, int ih, int iw, int oh, int ow, int kh,
+                     int kw, int padt, int padl, int strh, int strw)
+      : mb(mb), c(c), ih(ih), iw(iw), oh(oh), ow(ow), kh(kh), kw(kw),
+        padt(padt), padl(padl), strh(strh), strw(strw) {}
+};
+
+struct pool_bwd {
   size_t in, ichannel, iheight, iwidth;
   size_t wheight, wwidth;
   size_t vpadding, hpadding;
   size_t vstride, hstride;
   int on, ochannel, oheight, owidth;
 
-  test_pooling_t(size_t in, size_t ichannel, size_t iheight, size_t iwidth,
+  pool_bwd(size_t in, size_t ichannel, size_t iheight, size_t iwidth,
                  size_t wheight, size_t wwidth, size_t vpadding,
                  size_t hpadding, size_t vstride, size_t hstride)
       : in(in), ichannel(ichannel), iheight(iheight), iwidth(iwidth),
         wheight(wheight), wwidth(wwidth), vpadding(vpadding),
         hpadding(hpadding), vstride(vstride), hstride(hstride) {}
 
-  test_pooling_t(size_t in, size_t ichannel, size_t iheight, size_t iwidth,
+  pool_bwd(size_t in, size_t ichannel, size_t iheight, size_t iwidth,
                  size_t wheight, size_t wwidth, size_t vpadding,
                  size_t hpadding, size_t vstride, size_t hstride, size_t on,
                  size_t ochannel, size_t oheight, size_t owidth)
@@ -27,7 +42,42 @@ struct test_pooling_t {
 };
 
 template <typename dataType>
-void compute_hipdnn_pooling_backward(test_pooling_t &test_case, dataType *src,
+void hipdnn_maxpool_fwd(pool_fwd &c, dataType *src,
+                                dataType *dst) {
+
+  hipdnnHandle_t handle;
+  checkHIPDNN(hipdnnCreate(&handle));
+  hipdnnTensorDescriptor_t in_desc, out_desc;
+  checkHIPDNN(hipdnnCreateTensorDescriptor(&in_desc));
+  checkHIPDNN(hipdnnSetTensor4dDescriptor(
+      in_desc, HIPDNN_TENSOR_NCHW, HIPDNN_DATA_FLOAT, c.mb, c.c, c.ih, c.iw));
+
+  hipdnnPoolingDescriptor_t pool_desc;
+  checkHIPDNN(hipdnnCreatePoolingDescriptor(&pool_desc));
+  checkHIPDNN(hipdnnSetPooling2dDescriptor(pool_desc, HIPDNN_POOLING_MAX,
+                                           HIPDNN_NOT_PROPAGATE_NAN, c.kw, c.kh,
+                                           c.padt, c.padl, c.strh, c.strw));
+  checkHIPDNN(hipdnnGetPooling2dForwardOutputDim(pool_desc, in_desc, &c.mb,
+                                                 &c.c, &c.oh, &c.ow));
+
+  checkHIPDNN(hipdnnCreateTensorDescriptor(&out_desc));
+  checkHIPDNN(hipdnnSetTensor4dDescriptor(
+      out_desc, HIPDNN_TENSOR_NCHW, HIPDNN_DATA_FLOAT, c.mb, c.c, c.oh, c.ow));
+  float alpha = 1.f;
+  float beta = 0.f;
+
+  checkHIPDNN(hipdnnPoolingForward(handle, pool_desc, &alpha, in_desc, src,
+                                   &beta, out_desc, dst));
+
+  checkHIPDNN(hipdnnDestroyTensorDescriptor(in_desc));
+  checkHIPDNN(hipdnnDestroyTensorDescriptor(out_desc));
+  checkHIPDNN(hipdnnDestroyPoolingDescriptor(pool_desc));
+  checkHIPDNN(hipdnnDestroy(handle));
+}
+
+
+template <typename dataType>
+void hipdnn_pooling_backward(pool_bwd &test_case, dataType *src,
                                      dataType *grad, dataType *dst) {
 
   hipdnnHandle_t hipdnn;
@@ -60,15 +110,14 @@ void compute_hipdnn_pooling_backward(test_pooling_t &test_case, dataType *src,
 
   hipdnnPoolingForward(hipdnn, pool_desc, &alpha, in_desc, src, &beta, out_desc,
                        dst);
-
+  
   hipdnnPoolingBackward(hipdnn, pool_desc, &alpha, out_desc, dst, out_desc, dst,
                         in_desc, src, &beta, in_desc, grad);
 
- 
   hipdnnDestroyTensorDescriptor(out_desc);
   hipdnnDestroyPoolingDescriptor(pool_desc);
   hipdnnDestroyTensorDescriptor(in_desc);
   hipdnnDestroy(hipdnn);
 }
 
-#endif
+#endif // TEST_POOLING_COMMON_H
