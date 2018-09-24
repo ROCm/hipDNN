7861330ca07101b2a5ec6c3e9624dfd2f90bef2f
pradeepisro <pradeepisro49@gmail.com>
2018-09-24 11:09:57 +0530

    Fixed merge conflicts and added changes to the timer code

--- test/test_convolution_forward_backward.cpp
+++ test/test_convolution_forward_backward.cpp
@@ -1,19 +1,18 @@
 #include "test_convolution_common.hpp"
 
 TEST(convolution_fwd_bwd, func_test_fwd_bwd_convolution) {
-
-  Desc inputDesc(1, 3, 224, 224);
-    Desc filterDesc(21, 3, 3, 3);
+    Desc inputDesc(1, 3, 9, 9);
+    Desc filterDesc(1, 3, 3, 3);
     int pad[2] = {0, 0};    // zero padding
     int stride[2] = {1, 1}; // stride 1
+
     Desc outputDesc =
         calculateConv2DOutputDesc_int(inputDesc, filterDesc, pad, stride);
     Memory<float> srcData = createMemory<float>(inputDesc);
     Memory<float> gradData = createMemory<float>(filterDesc);
     Memory<float> dstDataGPU = createMemory<float>(outputDesc);
     Memory<float> filterData = createMemory<float>(filterDesc);
-    populateMemoryRandom<float>(srcData);
-    populateMemoryRandom<float>(filterData);
+    
     test_convolution_size testConvolutionSizes(
         inputDesc.N, 1, inputDesc.C, inputDesc.H, inputDesc.W, outputDesc.C,
         outputDesc.H, outputDesc.W, filterDesc.H, filterDesc.W, pad[0], pad[1],
@@ -21,7 +20,7 @@ TEST(convolution_fwd_bwd, func_test_fwd_bwd_convolution) {
 
   int ip_size[4] = {inputDesc.N, inputDesc.C, inputDesc.H, inputDesc.W};
   int k_size[4] = {filterDesc.N, filterDesc.C, filterDesc.H, filterDesc.W};
-  int op_size[4] =  {outputDesc.N, outputDesc.C, outputDesc.H, outputDesc.W};
+  int op_size[4] =  {filterDesc.N, filterDesc.C, filterDesc.H, filterDesc.W};
 
   std::string str_ip_size  = convert_to_string((int*)ip_size,4);
   std::string str_k_size  = convert_to_string((int*)k_size,4);
@@ -31,6 +30,8 @@ TEST(convolution_fwd_bwd, func_test_fwd_bwd_convolution) {
 
     std::vector<double> time_vector(benchmark_iterations,0);
     for(int i = 0; i < benchmark_iterations; i++){
+      populateMemoryRandom<float>(srcData);
+      populateMemoryRandom<float>(filterData);
       timer.restart();
 
       compute_hipdnn_conv_forward<float>(testConvolutionSizes, srcData.gpu(),
