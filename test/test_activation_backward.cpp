f058ff61ec1da3e862bb8070f180566172dac7fc
pradeepisro <pradeepisro49@gmail.com>
2018-09-25 12:10:51 +0530

    updated all unit tests with new avg_execution time calculation

--- test/test_activation_backward.cpp
+++ test/test_activation_backward.cpp
@@ -2,6 +2,8 @@
 
 TEST(activation_backward, func_test_naive_activation) {
   activation_params_t test_case(1, 1, 224, 244);
+  float avg_time = 0;
+
   Memory<float> dataSrc(test_case.n * test_case.channels * test_case.height *
                         test_case.width);
   Memory<float> dataGrad(test_case.n * test_case.channels * test_case.height *
@@ -18,21 +20,15 @@ TEST(activation_backward, func_test_naive_activation) {
   std::string str_k_size  = "NIL";
   std::string str_op_size  = convert_to_string((int*)op_size,4);
 
-  high_resolution_timer_t timer;
-    std::vector<double> time_vector(benchmark_iterations);
-    for(int i = 0; i < benchmark_iterations; i++){
-      timer.restart();
-      compute_hipdnn_activation_backward(test_case, dataSrc.gpu(), dataGrad.gpu(),
-                                     dataDst.gpu());
-      hipDeviceSynchronize();
-      std::uint64_t time_elapsed = timer.elapsed_nanoseconds();
-      time_vector[i] = (double)time_elapsed / 1000;
-    }
-    double avg_time = std::accumulate(time_vector.begin() + 10, time_vector.end(), 0.0) / (benchmark_iterations - 10);
-    std::cout << "Average Time: " << avg_time << std::endl;
+  compute_hipdnn_activation_backward<float>(test_case, dataSrc.gpu(), dataGrad.gpu(),
+                                     dataDst.gpu(),&avg_time);
+
+    std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
+
     std::string strt = "./result_unittest.csv";
     std::string testname = "activation_backward:func_test_naive_activation";
     float* temp = dataDst.getDataFromGPU();
+
     std::string str  = convert_to_string((float*)temp,(int)dataDst.get_num_elements());
     write_to_csv(strt, str, testname, avg_time, str_ip_size, str_k_size, str_op_size);
 
