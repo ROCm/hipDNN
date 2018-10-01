dd721a1021d6d66634b38e8c7eb467c6b0ac23ed
pradeepisro <pradeepisro49@gmail.com>
2018-10-01 11:31:25 +0530

    Cleaned up the code
    [ROCm/hipDNN commit: 039f3255206ba86a3923738a65f5b8c2c28c6e03]

--- library/include/hcc_detail/hipdnn_miopen.h
+++ library/include/hcc_detail/hipdnn_miopen.h
@@ -27,58 +27,6 @@
 
 #define HIPDNN_BN_MIN_EPSILON 1e-05
 
-// Defining structure for containing performance and algorithm results
-
-// struct hipdnnConvolutionFwdAlgoPerf_t {
-//     hipdnnConvolutionFwdAlgo_t algo;
-//     hipdnnStatus_t status;
-//     float time;
-//     size_t memory;
-//     long reserved[3];
-// };
-
-// struct hipdnnConvolutionBwdDataAlgoPerf_t {
-//     hipdnnConvolutionBwdDataAlgo_t algo;
-//     hipdnnStatus_t status;
-//     float time;
-//     size_t memory;
-//     long reserved[3];
-// };
-// struct hipdnnConvolutionBwdFilterAlgoPerf_t {
-//     hipdnnConvolutionBwdFilterAlgo_t algo;
-//     hipdnnStatus_t status;
-//     float time;
-//     size_t memory;
-//     long reserved[3];
-// };
-
-// typedef miopenTensorDescriptor_t hipdnnTensorDescriptor_t;
-
-// typedef miopenTensorDescriptor_t hipdnnReduceTensorDescriptor_t;
-
-// typedef miopenTensorDescriptor_t hipdnnFilterDescriptor_t;
-
-// typedef miopenConvolutionDescriptor_t hipdnnConvolutionDescriptor_t;
-
-// typedef miopenLRNDescriptor_t hipdnnLRNDescriptor_t;
-
-// typedef miopenActivationDescriptor_t hipdnnActivationDescriptor_t;
-
-// typedef miopenPoolingDescriptor_t hipdnnPoolingDescriptor_t;
-
-// // HGSOS cudnn makes a distinction
-// typedef miopenTensorOp_t hipdnnOpTensorDescriptor_t;
-
-// typedef miopenAcceleratorQueue_t hipdnnStream_t;
-
-// typedef miopenHandle_t hipdnnHandle_t;
-
-// typedef miopenTensorOp_t *hipdnnDropoutDescriptor_t;
-
-// typedef miopenRNNDescriptor_t hipdnnRNNDescriptor_t;
-
-// typedef miopenTensorOp_t *hipdnnPersistentRNNPlan_t;
-
 hipdnnStatus_t miopenTohipConvolutionFwdAlgo(miopenConvFwdAlgorithm_t in,
                                              hipdnnConvolutionFwdAlgo_t *out);
 
