c7c6ef6662be11887000196a222971f747eff4fd
sreelu1995 <pheonix116s@gmail.com>
2018-10-03 12:07:47 +0530

    added new function to support integrated unit tests combined dims dump

--- test/test_convolution_forward.cpp
+++ test/test_convolution_forward.cpp
@@ -18,7 +18,7 @@ TEST(convolution_fwd, func_check_zero_padding_medium_input_batch32) {
     test_convolution_sizes_t testConvolutionSizes(
         inputDesc.N, 1, inputDesc.C, inputDesc.H, inputDesc.W, outputDesc.C,
         outputDesc.H, outputDesc.W, filterDesc.H, filterDesc.W, pad[0], pad[1],
-        stride[0], stride[1], 1, 1);
+        stride[0], stride[1], 2, 2);
 
   int ip_size[4] = {inputDesc.N, inputDesc.C, inputDesc.H, inputDesc.W};
   int k_size[4] = {filterDesc.N, filterDesc.C, filterDesc.H, filterDesc.W};
@@ -198,4 +198,44 @@ std::string testname = "convolution_fwd:func_check_dilation1x1_batch64";
 float* temp = dstDataGPU.getDataFromGPU();
 std::string str = convert_to_string((float*)temp,(int)dstDataGPU.get_num_elements());
 write_to_csv(strt, str, testname,avg_time, str_ip_size, str_k_size, str_op_size);
+}
+
+TEST(convolution_fwd, func_check_zero_padding_medium_input_batch128) {
+    Desc inputDesc(128, 3, 16, 16);
+    Desc filterDesc(21, 3, 4, 4);
+    int pad[2] = {0, 0};    // zero padding
+    int stride[2] = {1, 1}; // stride 1
+    float avg_time = 0;
+
+    Desc outputDesc =
+        calculateConv2DOutputDesc(inputDesc, filterDesc, pad, stride);
+    Memory<float> srcData = createMemory<float>(inputDesc);
+    Memory<float> dstDataCPU = createMemory<float>(outputDesc);
+    Memory<float> dstDataGPU = createMemory<float>(outputDesc);
+    Memory<float> filterData = createMemory<float>(filterDesc);
+    populateMemoryRandom<float>(srcData);
+    populateMemoryRandom<float>(filterData);
+    test_convolution_sizes_t testConvolutionSizes(
+        inputDesc.N, 1, inputDesc.C, inputDesc.H, inputDesc.W, outputDesc.C,
+        outputDesc.H, outputDesc.W, filterDesc.H, filterDesc.W, pad[0], pad[1],
+        stride[0], stride[1], 1, 1);
+
+  int ip_size[4] = {inputDesc.N, inputDesc.C, inputDesc.H, inputDesc.W};
+  int k_size[4] = {filterDesc.N, filterDesc.C, filterDesc.H, filterDesc.W};
+  int op_size[4] =  {outputDesc.N, outputDesc.C, outputDesc.H, outputDesc.W};
+
+  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
+  std::string str_k_size  = convert_to_string((int*)k_size,4);
+  std::string str_op_size  = convert_to_string((int*)op_size,4);
+
+  compute_hipdnn_conv_fwd<float>(testConvolutionSizes, srcData.gpu(),
+                                filterData.gpu(), NULL, dstDataGPU.gpu(),&avg_time);
+     
+  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
+
+    std::string strt = "./result_unittest.csv";
+    std::string testname = "convolution_fwd:func_check_zero_padding_medium_input_batch128";
+    float* temp = dstDataGPU.getDataFromGPU();
+    std::string str  = convert_to_string((float*)temp,(int)dstDataGPU.get_num_elements());
+    write_to_csv(strt, str, testname, avg_time, str_ip_size, str_k_size, str_op_size);
 }
--- test/utils/csv/csv_integration.hpp
+++ test/utils/csv/csv_integration.hpp
@@ -26,6 +26,35 @@ std::string convert_to_string(dataType *arr, int s_size) {
   return str;
 }
 
+template <typename dataType>
+std::string integration_dims_to_string(dataType *arr1, dataType *arr2) {
+  std::ostringstream os;
+
+  os << "\"C:[";
+
+  for (int i = 0; i < 4; i++) {
+    os << arr1[i];
+
+    if (i < 3)
+      os << ",";
+  }
+
+  os << "] MP:[";
+
+  for (int i = 0; i < 4; i++) {
+    os << arr2[i];
+
+    if (i < 3)
+      os << ",";
+  }
+
+  os << "]\"";
+
+  std::string str(os.str());
+
+  return str;
+}
+
 void write_to_csv(std::string filename, std::string str, std::string testname,
                   float avg_time, std::string str_ip_size, 
                   std::string str_k_size, std::string str_op_size);
