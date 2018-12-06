c699eb4ab6ab744223f43afed185c277bf22333b
sreelu1995 <pheonix116s@gmail.com>
2018-12-06 16:27:12 +0530

    modified compare_results.py to calculate and display the first mismatch in every failed test case, total number of tests, number of tests passed and number of tests failed

--- test/utils/csv/compare_results.py
+++ test/utils/csv/compare_results.py
@@ -11,6 +11,8 @@ amd_op = []
 nv_op = []
 
 count = 0
+cnt_p = 0
+cnt_f = 0
 
 for filename1 in os.listdir(hcc_folder):
     for filename2 in os.listdir(nvcc_folder):
@@ -20,29 +22,44 @@ for filename1 in os.listdir(hcc_folder):
                     readCSV1 = csv.reader(csvfile1, delimiter=',')
                     readCSV2 = csv.reader(csvfile2, delimiter=',')
                     
+
+
                     for row1 in readCSV1:
                         for row2 in readCSV2:
-                            if (row1[0] == row2[0]):
+                           
+                           x = 0.0
+                           y = 0.0 
+   
+                           if (row1[0] == row2[0]):
                               count2 = 0
                               flag = "pass"
-                             # print "row1:",row1[0],"   ","row2:",row2[0]
+                             
                               for op1,op2 in zip(row1[1:],row2[1:]):
                                   if (op1=='' or op2==''):
-                                      break
-                                  #print op1,"  ", op2
+                                     break
+                                  
                                   n1 = float(op1)
                                   n2 = float(op2)
+                                                                    
                                   if (n1 != n2):
-                                     if ((n1 - n2) > 0.001):
+                                     if ((abs(n1 - n2)) > 0.001):
+                                        if (count2 == 0):
+                                            x = n1
+                                            y = n2
+
                                         count2 = count2 + 1
                                         flag = "fail"
-                              
+
                               if (flag == "pass") :       
-                                 print row1[0], "Test Passed"
+                                 print row1[0], ":   Test Passed!"
+                                 cnt_p = cnt_p + 1
                               else: 
-                                 print row1[0],"Test Failed",count2
+                                 print row1[0],"Test Failed! Total number of mismatches: ",count2,"  first mismatch: ",x," != ",y
+                                 cnt_f = cnt_f + 1
+
                               count = count + 1
                               break
                                                     
-print("count: ")
-print(count)
+print "Total number of tests: ", count
+print "Number of tests passed: ", cnt_p
+print "Number of tests failed: ", cnt_f
