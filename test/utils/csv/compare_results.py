3daad563f43ba3c89cd1929bd592df6316f74915
sreelu1995 <pheonix116s@gmail.com>
2018-12-07 17:39:07 +0530

    changed styling for compare_results
    [ROCm/hipDNN commit: 0e1fb36dcf9c79b161528f0c7c6abb08ed98c30d]

--- test/utils/csv/compare_results.py
+++ test/utils/csv/compare_results.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import sys
+from termcolor import colored
 
 csv.field_size_limit(sys.maxsize)
 
@@ -14,6 +15,10 @@ count = 0
 cnt_p = 0
 cnt_f = 0
 
+t_pass = colored('Test passed!', 'green', attrs=['bold'])
+t_fail = colored('Test failed!', 'red', attrs=['bold'])
+test_name = colored('Test name: ', 'blue', attrs=['bold'])
+
 for filename1 in os.listdir(hcc_folder):
     for filename2 in os.listdir(nvcc_folder):
        
@@ -21,9 +26,7 @@ for filename1 in os.listdir(hcc_folder):
 	       with open((hcc_folder+"/"+filename1),'rb') as csvfile1, open((nvcc_folder+"/"+filename2),'rb') as csvfile2:
                     readCSV1 = csv.reader(csvfile1, delimiter=',')
                     readCSV2 = csv.reader(csvfile2, delimiter=',')
-                    
-
-
+            
                     for row1 in readCSV1:
                         for row2 in readCSV2:
                            
@@ -32,7 +35,7 @@ for filename1 in os.listdir(hcc_folder):
    
                            if (row1[0] == row2[0]):
                               count2 = 0
-                              flag = "pass"
+                              flag = t_pass
                              
                               for op1,op2 in zip(row1[1:],row2[1:]):
                                   if (op1=='' or op2==''):
@@ -48,18 +51,23 @@ for filename1 in os.listdir(hcc_folder):
                                             y = n2
 
                                         count2 = count2 + 1
-                                        flag = "fail"
+                                        flag = t_fail
 
-                              if (flag == "pass") :       
-                                 print row1[0], ":   Test Passed!"
+                              if (flag == t_pass) :       
+                                 print test_name, row1[0]
+                                 print "  Test status: ",t_pass 
                                  cnt_p = cnt_p + 1
                               else: 
-                                 print row1[0],"Test Failed! Total number of mismatches: ",count2,"  first mismatch: ",x," != ",y
+                                 print test_name, row1[0]
+                                 print "  Test status: ", t_fail
+                                 print "  Total number of mismatches: ",count2
+                                 print "  First mismatch: ",x," != ",y
                                  cnt_f = cnt_f + 1
 
                               count = count + 1
+                              print "\n"
                               break
-                                                    
-print "Total number of tests: ", count
+                                           
+print "\nTotal number of tests: ", count
 print "Number of tests passed: ", cnt_p
 print "Number of tests failed: ", cnt_f
