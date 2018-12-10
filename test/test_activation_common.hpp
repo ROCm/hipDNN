9915eec0a704119a9a83df7994c9ce878736bdf0
sreelu1995 <pheonix116s@gmail.com>
2018-12-10 14:32:21 +0530

    activation unit test cleanup
    [ROCm/hipDNN commit: 406c66a6dd269243e3914bc00f5384a2f690a4cc]

--- test/test_activation_common.hpp
+++ test/test_activation_common.hpp
@@ -1,7 +1,71 @@
-#ifndef TEST_ACTIVATION_BACKWARD_HPP
-#define TEST_ACTIVATION_BACKWARD_HPP
-#include "common.hpp"
+#ifndef TEST_ACTIVATION_COMMON_HPP
+#define TEST_ACTIVATION_COMMON_HPP
+
 #include "hipdnn_test_common.h"
+#include "common.hpp"
+
+template <typename dataType>
+void compute_hipdnn_activation_forward(activation_params_t &test_case,
+                                        dataType *src,
+                                        dataType *dst, float *avg_time) {
+  hipdnnHandle_t hipdnn;
+  checkHIPDNN(hipdnnCreate(&hipdnn));
+
+  hipdnnTensorDescriptor_t in_desc;
+  checkHIPDNN(hipdnnCreateTensorDescriptor(&in_desc));
+
+  checkHIPDNN(hipdnnSetTensor4dDescriptor(
+      in_desc, HIPDNN_TENSOR_NCHW, HIPDNN_DATA_FLOAT, test_case.n,
+      test_case.channels, test_case.height, test_case.width));
+
+  hipdnnActivationDescriptor_t activationDesc;
+  //hipdnnActivationMode_t mode = HIPDNN_ACTIVATION_SIGMOID;
+  hipdnnActivationMode_t mode = HIPDNN_ACTIVATION_RELU;
+  hipdnnNanPropagation_t reluNanOpt = HIPDNN_NOT_PROPAGATE_NAN;
+
+  double reluCeilingOrAlpha = 0;
+  double activBeta = 0;
+  double activExp = 0;
+
+  checkHIPDNN(hipdnnCreateActivationDescriptor(&activationDesc));
+  checkHIPDNN(hipdnnSetActivationDescriptor(activationDesc, mode, reluNanOpt,
+                                            reluCeilingOrAlpha, activBeta,
+                                            activExp));
+
+  hipdnnTensorDescriptor_t out_desc;
+  checkHIPDNN(hipdnnCreateTensorDescriptor(&out_desc));
+  checkHIPDNN(hipdnnSetTensor4dDescriptor(
+               out_desc, HIPDNN_TENSOR_NCHW, HIPDNN_DATA_FLOAT, test_case.n,
+               test_case.channels, test_case.height, test_case.width));
+
+  float alpha = 1.f;
+  float beta = 0.f;
+
+  high_resolution_timer_t timer;
+
+  std::vector<double> time_vector(benchmark_iterations, 0);
+
+  for (int i = 0; i < benchmark_iterations; i++) {
+
+        timer.restart();
+
+        hipdnnActivationForward(hipdnn, activationDesc, &alpha, in_desc, src,
+                               &beta, out_desc, dst);
+        hipDeviceSynchronize();
+
+        std::uint64_t time_elapsed = timer.elapsed_nanoseconds();
+        time_vector[i] = (double)time_elapsed / 1000;
+    }
+
+  *avg_time = (float)std::accumulate(time_vector.begin() + 10,
+                                     time_vector.end(), 0)
+                                     / (benchmark_iterations - 10);
+
+  hipdnnDestroyTensorDescriptor(out_desc);
+  hipdnnDestroyActivationDescriptor(activationDesc);
+  hipdnnDestroyTensorDescriptor(in_desc);
+  hipdnnDestroy(hipdnn);
+}
 
 template <typename dataType>
 void compute_hipdnn_activation_backward(activation_params_t &test_case,
@@ -69,4 +133,4 @@ void compute_hipdnn_activation_backward(activation_params_t &test_case,
 
 }
 
-#endif //TEST_ACTIVATION_BACKWARD_HPP
+#endif //TEST_ACTIVATION_FORWARD_HPP
