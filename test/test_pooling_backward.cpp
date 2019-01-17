7514cbf98d50e287793d133f5f7ac1c786050db0
sreelu1995 <pheonix116s@gmail.com>
2019-01-17 15:46:47 +0530

    Remove unit tests with unsupported modes in pooling backward
    [ROCm/hipDNN commit: 53de60c634fbdd92cd365cc2fb1a89ecb60202cf]

--- test/test_pooling_backward.cpp
+++ test/test_pooling_backward.cpp
@@ -48,51 +48,6 @@ TEST(pooling_backward, func_check_pooling_stride_2x2) {
   dump_result_csv(filename, testname, temp, (int)dataGrad.get_num_elements());
 }
 
-TEST(pooling_backward, func_check_pooling_DETERMINISTIC) {
-
-  float avg_time = 0;
-  int oheight = 4, owidth = 4;
-
-  poolB_mode = HIPDNN_POOLING_MAX_DETERMINISTIC;
-
-  pool_bwd test_case(1, 1, 4, 4, 2, 2, 0, 0, 2, 2, 1, 1, oheight, owidth);
-
-  Memory<float> dataSrc(16);
-  Memory<float> dataGrad(16);
-
-  populateMemoryRandom(dataSrc);
-  populateMemoryRandom(dataGrad);
-
-  Memory<float> dataDst(test_case.on * test_case.ochannel * test_case.oheight *
-                        test_case.owidth);
-
-  int ip_size[4] = {1,1,4,4};
-  int k_size[4] = {1,1,2,2};
-  int op_size[4] =  {test_case.on, test_case.ochannel, test_case.oheight,
-                     test_case.owidth};
-
-  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
-  std::string str_k_size  = convert_to_string((int*)k_size,4);
-  std::string str_op_size  = convert_to_string((int*)op_size,4);
-
-  hipdnn_pooling_backward(test_case, dataSrc.gpu(), dataGrad.gpu(),
-                              dataDst.gpu(), poolB_mode, dataType_b, &avg_time);
-
-  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
-
-  float* temp = dataGrad.getDataFromGPU();
-
-  std::string str  = convert_to_string((float*)temp,
-                                       (int)dataGrad.get_num_elements());
-
-  std::string strt = "./result_unittest.csv";
-  std::string testname = "pooling_backward:func_check_pooling_DETERMINISTIC";
-  std::string filename="pooling_backward.csv";
-
-  write_to_csv(strt, str, testname,avg_time, str_ip_size, str_k_size, str_op_size);
-  dump_result_csv(filename, testname, temp, (int)dataGrad.get_num_elements());
-}
-
 TEST(pooling_backward, func_check_pooling_AVERAGE_COUNT_INCLUDE_PADDING) {
 
   float avg_time = 0;
@@ -132,51 +87,6 @@ TEST(pooling_backward, func_check_pooling_AVERAGE_COUNT_INCLUDE_PADDING) {
 
   std::string strt = "./result_unittest.csv";
   std::string testname = "pooling_backward:func_check_pooling_AVERAGE_COUNT_INCLUDE_PADDING";
-  std::string filename="pooling_backward.csv";
-
-  write_to_csv(strt, str, testname,avg_time, str_ip_size, str_k_size, str_op_size);
-  dump_result_csv(filename, testname, temp, (int)dataGrad.get_num_elements());
-}
-
-TEST(pooling_backward, func_check_pooling_AVERAGE_COUNT_EXCLUDE_PADDING) {
-
-  float avg_time = 0;
-  int oheight = 4, owidth = 4;
-
-  poolB_mode = HIPDNN_POOLING_AVERAGE_COUNT_EXCLUDE_PADDING;
-
-  pool_bwd test_case(1, 1, 4, 4, 2, 2, 0, 0, 2, 2, 1, 1, oheight, owidth);
-
-  Memory<float> dataSrc(16);
-  Memory<float> dataGrad(16);
-
-  populateMemoryRandom(dataSrc);
-  populateMemoryRandom(dataGrad);
-
-  Memory<float> dataDst(test_case.on * test_case.ochannel * test_case.oheight *
-                        test_case.owidth);
-
-  int ip_size[4] = {1,1,4,4};
-  int k_size[4] = {1,1,2,2};
-  int op_size[4] =  {test_case.on, test_case.ochannel, test_case.oheight,
-                     test_case.owidth};
-
-  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
-  std::string str_k_size  = convert_to_string((int*)k_size,4);
-  std::string str_op_size  = convert_to_string((int*)op_size,4);
-
-  hipdnn_pooling_backward(test_case, dataSrc.gpu(), dataGrad.gpu(),
-                              dataDst.gpu(), poolB_mode, dataType_b, &avg_time);
-
-  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
-
-  float* temp = dataGrad.getDataFromGPU();
-
-  std::string str  = convert_to_string((float*)temp,
-                                       (int)dataGrad.get_num_elements());
-
-  std::string strt = "./result_unittest.csv";
-  std::string testname = "pooling_backward:func_check_pooling_AVERAGE_COUNT_EXCLUDE_PADDING";
   std::string filename="pooling_backward.csv";
 
   write_to_csv(strt, str, testname,avg_time, str_ip_size, str_k_size, str_op_size);
