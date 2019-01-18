c665e48e7a348b36424eb088516edda0bd2e351c
sreelu1995 <pheonix116s@gmail.com>
2019-01-18 17:01:55 +0530

    Add unit tests with rectangular dimensions for pooling forward

--- test/test_pooling_forward.cpp
+++ test/test_pooling_forward.cpp
@@ -13,7 +13,6 @@ TEST(pooling_fwd, func_check_zero_padding) {
 
   Desc outputDesc = calculate_pool_Dims(inputDesc, spatial_ext, pad, stride);
 
-  std::cout<<"\n output Dims: "<<outputDesc.H <<"\t"<< outputDesc.W;
   Memory<float> srcData = createMemory<float>(inputDesc);
   Memory<float> dstDataGPU = createMemory<float>(outputDesc);
   populateMemoryRandom<float>(srcData);
@@ -65,8 +64,6 @@ TEST(pooling_fwd, func_check_AVERAGE_COUNT_INCLUDE_PADDING) {
   int pad[2] = {2, 2};
 
   Desc outputDesc = calculate_pool_Dims(inputDesc, spatial_ext, pad, stride);
-
-  std::cout<<"\n output Dims: "<<outputDesc.H <<"\t"<< outputDesc.W;
   Memory<float> srcData = createMemory<float>(inputDesc);
   Memory<float> dstDataGPU = createMemory<float>(outputDesc);
   populateMemoryRandom<float>(srcData);
@@ -120,8 +117,6 @@ TEST(pooling_fwd, func_check_batch32) {
   int pad[2] = {0, 0};
 
   Desc outputDesc = calculate_pool_Dims(inputDesc, spatial_ext, pad, stride);
-
-  std::cout<<"\n output Dims: "<<outputDesc.H <<"\t"<< outputDesc.W;
   Memory<float> srcData = createMemory<float>(inputDesc);
   Memory<float> dstDataGPU = createMemory<float>(outputDesc);
   populateMemoryRandom<float>(srcData);
@@ -262,6 +257,110 @@ TEST(pooling_fwd, func_check_batch128) {
   write_to_csv(strt, str, testname, avg_time, str_ip_size, str_k_size, str_op_size);
   dump_result_csv(filename, testname, temp, (int)dstDataGPU.get_num_elements());
 }
+
+TEST(pooling_fwd, func_check_rectangular_dims_small_size) {
+
+  Desc inputDesc(1, 1, 4, 4);
+  pool_mode = HIPDNN_POOLING_MAX;
+  int spatial_ext[2] = {2, 1};
+  int stride[2] = {2, 1};
+  int pad[2] = {0,0};
+
+  Desc outputDesc = calculate_pool_Dims(inputDesc, spatial_ext, pad, stride);
+  Memory<float> srcData = createMemory<float>(inputDesc);
+  Memory<float> dstDataGPU = createMemory<float>(outputDesc);
+  populateMemoryRandom<float>(srcData);
+
+  test_pooling_descriptor pool(inputDesc.N, inputDesc.C, inputDesc.H, inputDesc.W,
+                               outputDesc.H, outputDesc.W, spatial_ext[0],
+                               spatial_ext[1], pad[0], pad[1], stride[0],
+                               stride[1]);
+
+  alpha = 1.f;
+  beta = 0.f;
+
+  float avg_time = 0;
+
+  int ip_size[4] = {pool.mb, pool.c, pool.ih, pool.iw};
+  int k_size[4] = {pool.mb, pool.c, pool.kh, pool.kw};
+  int op_size[4] =  {pool.mb, pool.c, pool.oh, pool.ow};
+
+  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
+  std::string str_k_size  = convert_to_string((int*)k_size,4);
+  std::string str_op_size  = convert_to_string((int*)op_size,4);
+
+  hipdnn_pooling_forward<float>(pool, srcData.gpu(), dstDataGPU.gpu(), pool_mode,
+                                dataType, false, alpha, beta, &avg_time);
+
+  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
+
+  float* temp2 = srcData.getDataFromGPU();
+  std::string ip = convert_to_string((float*)temp2,(int)srcData.get_num_elements());
+
+  float* temp = dstDataGPU.getDataFromGPU();
+
+  std::string strt = "./result_unittest.csv";
+  std::string testname = "pooling_fwd:func_check_rectangular_dims_small_size";
+  std::string filename="pooling_forward.csv";
+
+  std::string str  = convert_to_string((float*)temp,(int)dstDataGPU.get_num_elements());
+
+  write_to_csv(strt, str, testname, avg_time, str_ip_size, str_k_size, str_op_size);
+  dump_result_csv(filename, testname, temp, (int)dstDataGPU.get_num_elements());
+}
+
+TEST(pooling_fwd, func_check_rectangular_dims_medium_size) {
+
+  Desc inputDesc(32, 1, 64, 64);
+  pool_mode = HIPDNN_POOLING_MAX;
+  int spatial_ext[2] = {4, 2};
+  int stride[2] = {4, 2};
+  int pad[2] = {0,0};
+
+  Desc outputDesc = calculate_pool_Dims(inputDesc, spatial_ext, pad, stride);
+
+  Memory<float> srcData = createMemory<float>(inputDesc);
+  Memory<float> dstDataGPU = createMemory<float>(outputDesc);
+  populateMemoryRandom<float>(srcData);
+
+  test_pooling_descriptor pool(inputDesc.N, inputDesc.C, inputDesc.H, inputDesc.W,
+                               outputDesc.H, outputDesc.W, spatial_ext[0],
+                               spatial_ext[1], pad[0], pad[1], stride[0],
+                               stride[1]);
+
+  alpha = 1.f;
+  beta = 0.f;
+
+  float avg_time = 0;
+
+  int ip_size[4] = {pool.mb, pool.c, pool.ih, pool.iw};
+  int k_size[4] = {pool.mb, pool.c, pool.kh, pool.kw};
+  int op_size[4] =  {pool.mb, pool.c, pool.oh, pool.ow};
+
+  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
+  std::string str_k_size  = convert_to_string((int*)k_size,4);
+  std::string str_op_size  = convert_to_string((int*)op_size,4);
+
+  hipdnn_pooling_forward<float>(pool, srcData.gpu(), dstDataGPU.gpu(), pool_mode,
+                                dataType, false, alpha, beta, &avg_time);
+
+  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
+
+  float* temp2 = srcData.getDataFromGPU();
+  std::string ip = convert_to_string((float*)temp2,(int)srcData.get_num_elements());
+
+  float* temp = dstDataGPU.getDataFromGPU();
+
+  std::string strt = "./result_unittest.csv";
+  std::string testname = "pooling_fwd:func_check_rectangular_dims_medium_size";
+  std::string filename="pooling_forward.csv";
+
+  std::string str  = convert_to_string((float*)temp,(int)dstDataGPU.get_num_elements());
+
+  write_to_csv(strt, str, testname, avg_time, str_ip_size, str_k_size, str_op_size);
+  dump_result_csv(filename, testname, temp, (int)dstDataGPU.get_num_elements());
+}
+
 /*
 //half2
 
