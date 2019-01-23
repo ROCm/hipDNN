14acb88833dcbbc47518e212da3a9443b5dd07ea
sreelu1995 <pheonix116s@gmail.com>
2019-01-23 19:25:24 +0530

    Clean up activation tests 1: Remove redundancy in code and make modes global

--- test/test_activation_backward.cpp
+++ test/test_activation_backward.cpp
@@ -1,307 +1,69 @@
 #include "test_activation_common.hpp"
 
-hipdnnActivationMode_t actB_mode;
+// alpha_beta[4] = {0.f, 0.5f, 1.f, 2.f};
 
 TEST(activation_backward, func_test_activation_RELU) {
 
+  // alpha = 1.f, beta = 0.f;
   activation_params_t test_case(1, 1, 224, 244);
-  float avg_time = 0;
-  alpha = 1.f;
-  beta = 0.f;
-  actB_mode = HIPDNN_ACTIVATION_RELU;
+  act_mode = HIPDNN_ACTIVATION_RELU;
 
-  Memory<float> dataSrc(test_case.n * test_case.channels * test_case.height *
-                        test_case.width);
-  Memory<float> dataGrad(test_case.n * test_case.channels * test_case.height *
-                         test_case.width);
-  Memory<float> dataDst(test_case.n * test_case.channels * test_case.height *
-                        test_case.width);
-
-  populateMemoryRandom(dataSrc);
-  populateMemoryRandom(dataGrad);
-
-  int ip_size[4] = {test_case.n, test_case.channels, test_case.height,
-                    test_case.width};
-  int op_size[4] = {test_case.n, test_case.channels, test_case.height,
-                    test_case.width};
-
-  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
-  std::string str_k_size  = "NIL";
-  std::string str_op_size  = convert_to_string((int*)op_size,4);
-
-  compute_hipdnn_activation_forward(test_case, dataSrc.gpu(), dataDst.gpu(),
-                                    actB_mode, alpha, beta, &avg_time);
-
-  compute_hipdnn_activation_backward<float>(test_case, dataSrc.gpu(),
-                                            dataGrad.gpu(), dataDst.gpu(),
-                                            actB_mode, alpha, beta, &avg_time);
-
-  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
-
-  std::string strt = "./result_unittest.csv";
-  std::string testname = "activation_backward:func_test_activation_RELU";
-  std::string filename="activation_backward.csv";
-
-  float* temp = dataDst.getDataFromGPU();
-
-  std::string str  = convert_to_string((float*)temp,
-                                       (int)dataDst.get_num_elements());
-
-  write_to_csv(strt, str, testname, avg_time, str_ip_size, str_k_size,
-               str_op_size);
-  dump_result_csv(filename, testname, temp, (int)dataDst.get_num_elements());
-
+  Test_activation_bwd<float>(test_case, act_mode,
+                     "activation_backward:func_test_activation_RELU",
+                     alpha_beta[2], alpha_beta[0]);
 }
 
 TEST(activation_backward, func_test_activation_SIGMOID) {
 
+  // alpha = 1.f, beta = 0.5f;
   activation_params_t test_case(1, 1, 224, 244);
-  float avg_time = 0;
-  alpha = 1.f;
-  beta = 0.5f;
-  actB_mode = HIPDNN_ACTIVATION_SIGMOID;
+  act_mode = HIPDNN_ACTIVATION_SIGMOID;
 
-  Memory<float> dataSrc(test_case.n * test_case.channels * test_case.height *
-                        test_case.width);
-  Memory<float> dataGrad(test_case.n * test_case.channels * test_case.height *
-                         test_case.width);
-  Memory<float> dataDst(test_case.n * test_case.channels * test_case.height *
-                        test_case.width);
-
-  populateMemoryRandom(dataSrc);
-  populateMemoryRandom(dataGrad);
-
-  int ip_size[4] = {test_case.n, test_case.channels, test_case.height,
-                    test_case.width};
-  int op_size[4] = {test_case.n, test_case.channels, test_case.height,
-                    test_case.width};
-
-  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
-  std::string str_k_size  = "NIL";
-  std::string str_op_size  = convert_to_string((int*)op_size,4);
-
-  compute_hipdnn_activation_forward(test_case, dataSrc.gpu(), dataDst.gpu(),
-                                    actB_mode, alpha, beta, &avg_time);
-
-  compute_hipdnn_activation_backward<float>(test_case, dataSrc.gpu(),
-                                            dataGrad.gpu(), dataDst.gpu(),
-                                            actB_mode, alpha, beta, &avg_time);
-
-  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
-
-  std::string strt = "./result_unittest.csv";
-  std::string testname = "activation_backward:func_test_activation_SIGMOID";
-  std::string filename="activation_backward.csv";
-
-  float* temp = dataDst.getDataFromGPU();
-
-  std::string str  = convert_to_string((float*)temp,
-                                       (int)dataDst.get_num_elements());
-
-  write_to_csv(strt, str, testname, avg_time, str_ip_size, str_k_size,
-               str_op_size);
-  dump_result_csv(filename, testname, temp, (int)dataDst.get_num_elements());
-
+  Test_activation_bwd<float>(test_case, act_mode,
+                     "activation_backward:func_test_activation_SIGMOID",
+                     alpha_beta[2], alpha_beta[1]);
 }
 
 TEST(activation_backward, func_test_activation_TANH) {
 
   activation_params_t test_case(1, 1, 224, 244);
-  float avg_time = 0;
-  alpha = 2.f;
-  beta = 0.f;
-  actB_mode = HIPDNN_ACTIVATION_TANH;
+  // alpha = 2.f, beta = 0.f;
+  act_mode = HIPDNN_ACTIVATION_TANH;
 
-  Memory<float> dataSrc(test_case.n * test_case.channels * test_case.height *
-                        test_case.width);
-  Memory<float> dataGrad(test_case.n * test_case.channels * test_case.height *
-                         test_case.width);
-  Memory<float> dataDst(test_case.n * test_case.channels * test_case.height *
-                        test_case.width);
-
-  populateMemoryRandom(dataSrc);
-  populateMemoryRandom(dataGrad);
-
-  int ip_size[4] = {test_case.n, test_case.channels, test_case.height,
-                    test_case.width};
-  int op_size[4] = {test_case.n, test_case.channels, test_case.height,
-                    test_case.width};
-
-  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
-  std::string str_k_size  = "NIL";
-  std::string str_op_size  = convert_to_string((int*)op_size,4);
-
-  compute_hipdnn_activation_forward(test_case, dataSrc.gpu(), dataDst.gpu(),
-                                    actB_mode, alpha, beta, &avg_time);
-
-  compute_hipdnn_activation_backward<float>(test_case, dataSrc.gpu(),
-                                            dataGrad.gpu(), dataDst.gpu(),
-                                            actB_mode, alpha, beta, &avg_time);
-
-  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
-
-  std::string strt = "./result_unittest.csv";
-  std::string testname = "activation_backward:func_test_activation_TANH";
-  std::string filename="activation_backward.csv";
-
-  float* temp = dataDst.getDataFromGPU();
-
-  std::string str  = convert_to_string((float*)temp,
-                                       (int)dataDst.get_num_elements());
-
-  write_to_csv(strt, str, testname, avg_time, str_ip_size, str_k_size,
-               str_op_size);
-  dump_result_csv(filename, testname, temp, (int)dataDst.get_num_elements());
-
+  Test_activation_bwd<float>(test_case, act_mode,
+                     "activation_backward:func_test_activation_TANH",
+                     alpha_beta[3], alpha_beta[0]);
 }
 
 TEST(activation_backward, func_test_activation_batch32) {
 
   activation_params_t test_case(32, 1, 224, 244);
-  float avg_time = 0;
-  alpha = 2.f;
-  beta = 1.f;
-  actB_mode = HIPDNN_ACTIVATION_RELU;
+  // alpha = 2.f, beta = 1.f;
+  act_mode = HIPDNN_ACTIVATION_RELU;
 
-  Memory<float> dataSrc(test_case.n * test_case.channels * test_case.height *
-                        test_case.width);
-  Memory<float> dataGrad(test_case.n * test_case.channels * test_case.height *
-                         test_case.width);
-  Memory<float> dataDst(test_case.n * test_case.channels * test_case.height *
-                        test_case.width);
-
-  populateMemoryRandom(dataSrc);
-  populateMemoryRandom(dataGrad);
-
-  int ip_size[4] = {test_case.n, test_case.channels, test_case.height,
-                    test_case.width};
-  int op_size[4] =  {test_case.n, test_case.channels, test_case.height,
-                     test_case.width};
-
-  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
-  std::string str_k_size  = "NIL";
-  std::string str_op_size  = convert_to_string((int*)op_size,4);
-
-  compute_hipdnn_activation_forward(test_case, dataSrc.gpu(), dataDst.gpu(),
-                                    actB_mode, alpha, beta, &avg_time);
-
-  compute_hipdnn_activation_backward<float>(test_case, dataSrc.gpu(),
-                                            dataGrad.gpu(), dataDst.gpu(),
-                                            actB_mode, alpha, beta, &avg_time);
-
-  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
-
-  std::string strt = "./result_unittest.csv";
-  std::string testname = "activation_backward:func_test_activation_batch32";
-  std::string filename="activation_backward.csv";
-
-  float* temp = dataDst.getDataFromGPU();
-
-  std::string str  = convert_to_string((float*)temp,
-                                       (int)dataDst.get_num_elements());
-
-  write_to_csv(strt, str, testname, avg_time, str_ip_size,
-               str_k_size, str_op_size);
-  dump_result_csv(filename, testname, temp, (int)dataDst.get_num_elements());
-
+  Test_activation_bwd<float>(test_case, act_mode,
+                     "activation_backward:func_test_activation_batch32",
+                     alpha_beta[3], alpha_beta[2]);
 }
 
 TEST(activation_backward, func_test_activation_batch64) {
 
   activation_params_t test_case(64, 1, 224, 244);
-  float avg_time = 0;
-  alpha = 0.5f;
-  beta = 0.f;
-  actB_mode = HIPDNN_ACTIVATION_RELU;
+  // alpha = 0.5f, beta = 0.f;
+  act_mode = HIPDNN_ACTIVATION_RELU;
 
-  Memory<float> dataSrc(test_case.n * test_case.channels * test_case.height *
-                        test_case.width);
-  Memory<float> dataGrad(test_case.n * test_case.channels * test_case.height *
-                         test_case.width);
-  Memory<float> dataDst(test_case.n * test_case.channels * test_case.height *
-                        test_case.width);
-
-  populateMemoryRandom(dataSrc);
-  populateMemoryRandom(dataGrad);
-
-  int ip_size[4] = {test_case.n, test_case.channels, test_case.height,
-                    test_case.width};
-  int op_size[4] =  {test_case.n, test_case.channels, test_case.height,
-                     test_case.width};
-
-  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
-  std::string str_k_size  = "NIL";
-  std::string str_op_size  = convert_to_string((int*)op_size,4);
-
-  compute_hipdnn_activation_forward(test_case, dataSrc.gpu(), dataDst.gpu(),
-                                    actB_mode, alpha, beta, &avg_time);
-
-  compute_hipdnn_activation_backward<float>(test_case, dataSrc.gpu(),
-                                            dataGrad.gpu(), dataDst.gpu(),
-                                            actB_mode, alpha, beta, &avg_time);
-
-  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
-
-  std::string strt = "./result_unittest.csv";
-  std::string testname = "activation_backward:func_test_activation_batch64";
-  std::string filename="activation_backward.csv";
-
-  float* temp = dataDst.getDataFromGPU();
-
-  std::string str  = convert_to_string((float*)temp,
-                                       (int)dataDst.get_num_elements());
-
-  write_to_csv(strt, str, testname, avg_time, str_ip_size,
-               str_k_size, str_op_size);
-  dump_result_csv(filename, testname, temp, (int)dataDst.get_num_elements());
-
+  Test_activation_bwd<float>(test_case, act_mode,
+                     "activation_backward:func_test_activation_batch32",
+                     alpha_beta[1], alpha_beta[0]);
 }
 
 TEST(activation_backward, func_test_activation_batch128) {
 
   activation_params_t test_case(128, 1, 4, 4);
-  float avg_time = 0;
-  alpha = 2.f;
-  beta = 0.5f;
-  actB_mode = HIPDNN_ACTIVATION_RELU;
+  // alpha = 2.f, beta = 0.5f;
+  act_mode = HIPDNN_ACTIVATION_RELU;
 
-  Memory<float> dataSrc(test_case.n * test_case.channels * test_case.height *
-                        test_case.width);
-  Memory<float> dataGrad(test_case.n * test_case.channels * test_case.height *
-                         test_case.width);
-  Memory<float> dataDst(test_case.n * test_case.channels * test_case.height *
-                        test_case.width);
-  populateMemoryRandom(dataSrc);
-  populateMemoryRandom(dataGrad);
-
-  int ip_size[4] = {test_case.n, test_case.channels, test_case.height,
-                    test_case.width};
-  int op_size[4] = {test_case.n, test_case.channels, test_case.height,
-                    test_case.width};
-
-  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
-  std::string str_k_size  = "NIL";
-  std::string str_op_size  = convert_to_string((int*)op_size,4);
-
-  compute_hipdnn_activation_forward(test_case, dataSrc.gpu(), dataDst.gpu(),
-                                    actB_mode, alpha, beta, &avg_time);
-
-  compute_hipdnn_activation_backward<float>(test_case, dataSrc.gpu(),
-                                            dataGrad.gpu(), dataDst.gpu(),
-                                            actB_mode, alpha, beta, &avg_time);
-
-  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
-
-  std::string strt = "./result_unittest.csv";
-  std::string testname = "activation_backward:func_test_activation_batch128";
-  std::string filename="activation_backward.csv";
-
-  float* temp = dataDst.getDataFromGPU();
-
-  std::string str  = convert_to_string((float*)temp,
-                                       (int)dataDst.get_num_elements());
-
-  write_to_csv(strt, str, testname, avg_time, str_ip_size, str_k_size,
-               str_op_size);
-  dump_result_csv(filename, testname, temp, (int)dataDst.get_num_elements());
+  Test_activation_bwd<float>(test_case, act_mode,
+                     "activation_backward:func_test_activation_batch32",
+                     alpha_beta[3], alpha_beta[1]);
 }
--- test/test_activation_common.hpp
+++ test/test_activation_common.hpp
@@ -127,4 +127,93 @@ void compute_hipdnn_activation_backward(activation_params_t &test_case,
 
 }
 
+template <typename dataType>
+void Test_activation_fwd(activation_params_t test_case,
+                         hipdnnActivationMode_t act_mode, std::string testname,
+                         float alpha, float beta)
+{
+  float avg_time = 0;
+
+  Memory<float> dataSrc(test_case.n * test_case.channels * test_case.height *
+                        test_case.width);
+  Memory<float> dataDst(test_case.n * test_case.channels * test_case.height *
+                        test_case.width);
+
+  populateMemoryRandom(dataSrc);
+
+  int ip_size[4] = {test_case.n, test_case.channels, test_case.height,
+                    test_case.width};
+  int op_size[4] = {test_case.n, test_case.channels, test_case.height,
+                    test_case.width};
+
+  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
+  std::string str_k_size  = "NIL";
+  std::string str_op_size  = convert_to_string((int*)op_size,4);
+
+  compute_hipdnn_activation_forward(test_case, dataSrc.gpu(), dataDst.gpu(),
+                                    act_mode, alpha, beta, &avg_time);
+
+  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
+
+  float* temp = dataDst.getDataFromGPU();
+
+  std::string strt = "./result_unittest.csv";
+  std::string filename = "activation_forward.csv";
+
+  std::string str  = convert_to_string((float*)temp,
+                                         (int)dataDst.get_num_elements());
+
+  write_to_csv(strt, str, testname, avg_time, str_ip_size, str_k_size,
+                 str_op_size);
+  dump_result_csv(filename, testname, temp, (int)dataDst.get_num_elements());
+}
+
+template <typename dataType>
+void Test_activation_bwd(activation_params_t test_case,
+                         hipdnnActivationMode_t act_mode, std::string testname,
+                         float alpha, float beta)
+{
+  float avg_time = 0;
+
+  Memory<dataType> dataSrc(test_case.n * test_case.channels * test_case.height *
+                        test_case.width);
+  Memory<dataType> dataGrad(test_case.n * test_case.channels * test_case.height *
+                         test_case.width);
+  Memory<dataType> dataDst(test_case.n * test_case.channels * test_case.height *
+                        test_case.width);
+
+  populateMemoryRandom(dataSrc);
+  populateMemoryRandom(dataGrad);
+
+  int ip_size[4] = {test_case.n, test_case.channels, test_case.height,
+                    test_case.width};
+  int op_size[4] = {test_case.n, test_case.channels, test_case.height,
+                    test_case.width};
+
+  std::string str_ip_size  = convert_to_string((int*)ip_size,4);
+  std::string str_k_size  = "NIL";
+  std::string str_op_size  = convert_to_string((int*)op_size,4);
+
+  compute_hipdnn_activation_forward(test_case, dataSrc.gpu(), dataDst.gpu(),
+                                    act_mode, alpha, beta, &avg_time);
+
+  compute_hipdnn_activation_backward<dataType>(test_case, dataSrc.gpu(),
+                                            dataGrad.gpu(), dataDst.gpu(),
+                                            act_mode, alpha, beta, &avg_time);
+
+  std::cout << "\nAverage Time is: " << avg_time << "micro seconds"<<std::endl;
+
+  std::string strt = "./result_unittest.csv";
+  std::string filename="activation_backward.csv";
+
+  float* temp = dataDst.getDataFromGPU();
+
+  std::string str  = convert_to_string((float*)temp,
+                                       (int)dataDst.get_num_elements());
+
+  write_to_csv(strt, str, testname, avg_time, str_ip_size, str_k_size,
+               str_op_size);
+  dump_result_csv(filename, testname, temp, (int)dataDst.get_num_elements());
+}
+
 #endif //TEST_ACTIVATION_FORWARD_HPP
