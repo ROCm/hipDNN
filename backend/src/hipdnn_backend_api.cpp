05e1c4a315053c6131c3ed2b6258d06c40d220a5
Adam Dickin <adam.dickin@amd.com>
2025-04-03 21:42:58 +0000

    remove extra bad code
    [ROCm/hipDNN commit: 5d816c65bb3006ddf10078f44ce0055d475f367e]

--- backend/src/hipdnn_backend_api.cpp
+++ backend/src/hipdnn_backend_api.cpp
@@ -2,13 +2,9 @@
 #include <iostream>
 
 #include "hello_world.hpp"
-#include <string.h>
 
 int publicFunctionHello()
 {
     std::cout << hipdnn_backend::HelloWorld::getMessage() << std::endl;
-
-    char buffer[10];
-    strcpy(buffer, "This string is too long for the buffer");
     return 1337;
 }
