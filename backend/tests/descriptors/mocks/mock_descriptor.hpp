8bf6d8b75c8a63a42dab30bf8718b68cc14e82dd
JonathanLichtnerAMD <195780826+JonathanLichtnerAMD@users.noreply.github.com>
2025-05-21 09:12:46 -0600

    Use gmocks for backend descriptor mocks (#54)
    * Link gmock with the backend unit tests
    
    * Replace custom backend descriptor mocks with a gmock implementation
    
    [ROCm/hipDNN commit: 50f7ea9089e3ca2a0471ba44d1a80dbe1df0d122]

--- backend/tests/descriptors/mocks/mock_descriptor.hpp
+++ backend/tests/descriptors/mocks/mock_descriptor.hpp
@@ -5,41 +5,42 @@
 
 #include "descriptors/backend_descriptor.hpp"
 
-#include <map>
-#include <vector>
+#include <gmock/gmock.h>
 
 namespace hipdnn_backend
 {
 
 class Mock_descriptor : public hipdnnBackendDescriptor
 {
-private:
-    std::map<std::pair<hipdnnBackendAttributeName_t, hipdnnBackendAttributeType_t>,
-             std::vector<const void*>>
-        _attributes;
-
 public:
-    Mock_descriptor(hipdnnBackendDescriptorType_t desc_type = HIPDNN_INVALID_TYPE,
-                    bool finalized = false);
+    Mock_descriptor(hipdnnBackendDescriptorType_t desc_type = HIPDNN_INVALID_TYPE)
+    {
+        type = desc_type;
+    }
     ~Mock_descriptor() override = default;
 
-    void set_data(hipdnnBackendAttributeName_t attribute_name,
-                  hipdnnBackendAttributeType_t attribute_type,
-                  int64_t element_count,
-                  const void* elements);
-
-    void finalize() override;
-
-    void get_attribute(hipdnnBackendAttributeName_t attribute_name,
-                       hipdnnBackendAttributeType_t attribute_type,
-                       int64_t requested_element_count,
-                       int64_t* element_count,
-                       void* array_of_elements) override;
-
-    void set_attribute(hipdnnBackendAttributeName_t attribute_name,
-                       hipdnnBackendAttributeType_t attribute_type,
-                       int64_t element_count,
-                       const void* array_of_elements) override;
+    MOCK_METHOD(void, finalize, (), (override));
+    MOCK_METHOD(bool, is_finalized, (), (const, override));
+    MOCK_METHOD(void,
+                set_attribute,
+                (hipdnnBackendAttributeName_t attribute_name,
+                 hipdnnBackendAttributeType_t attribute_type,
+                 int64_t element_count,
+                 const void* array_of_element),
+                (override));
+    MOCK_METHOD(void,
+                get_attribute,
+                (hipdnnBackendAttributeName_t attribute_name,
+                 hipdnnBackendAttributeType_t attribute_type,
+                 int64_t requested_element_count,
+                 int64_t* element_count,
+                 void* array_of_elements),
+                (override));
 };
+
+ACTION_P(SetArg4ToInt64, value) // NOLINT
+{
+    *static_cast<int64_t*>(arg4) = value;
+}
 
 } // namespace hipdnn_backend
