114acadec4711f2e748570361c9a32196d556fcf
Samuel Reeder <samuel.reeder@amd.com>
2025-07-17 21:17:52 +0000

    Add back plugin test logging

--- plugins/miopen_legacy_plugin/tests/main.cpp
+++ plugins/miopen_legacy_plugin/tests/main.cpp
@@ -4,11 +4,14 @@ SPDX-License-Identifier: MIT
 */
 
 #include <gtest/gtest.h>
+#include <hipdnn_backend.h>
 #include <hipdnn_sdk/logging/logger.hpp>
 
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
+
+    hipdnn::logging::initialize_callback_logging("miopen_legacy_plugin", hipdnnLoggingCallback_ext);
 
     return RUN_ALL_TESTS();
 }
