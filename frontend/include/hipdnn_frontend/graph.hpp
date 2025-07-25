e6355d8e6a2b36a3070577f046650c9db9c0463d
Adam Dickin <adam.dickin@amd.com>
2025-07-25 21:15:02 +0000

    fix some build warnings as errors and turn off 1 readability warning as we dont want to force this one.

--- frontend/include/hipdnn_frontend/graph.hpp
+++ frontend/include/hipdnn_frontend/graph.hpp
@@ -104,7 +104,7 @@ private:
         int required_count = 1;
         std::vector<std::unique_ptr<Scoped_hipdnn_backend_descriptor>> engine_configs;
         std::vector<hipdnnBackendDescriptor_t> engine_configs_shallow;
-        for(size_t i = 0; i < static_cast<size_t>(required_count); ++i)
+        for(size_t i = 0; std::cmp_less(i, required_count); ++i)
         {
             auto engine_cfg_desc = std::make_unique<Scoped_hipdnn_backend_descriptor>(
                 HIPDNN_BACKEND_ENGINECFG_DESCRIPTOR);
