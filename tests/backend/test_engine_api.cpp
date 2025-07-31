8955f84d09d4def0a2d64323e3a0b6b0db82352e
Brian Harrison <brian.harrison@amd.com>
2025-07-31 17:23:55 +0000

    Fix engine API tests due to fake engine Ids now being -1

--- tests/backend/test_engine_api.cpp
+++ tests/backend/test_engine_api.cpp
@@ -62,7 +62,7 @@ TEST_F(Engine_api_tests, SetEngineGraph)
 
 TEST_F(Engine_api_tests, SetEngineGlobalIndex)
 {
-    int64_t gidx = 0;
+    int64_t gidx = HIPDNN_TEST_PLUGIN_ID;
 
     EXPECT_EQ(hipdnnBackendSetAttribute(
                   _engine, HIPDNN_ATTR_ENGINE_GLOBAL_INDEX, HIPDNN_TYPE_INT64, 1, nullptr),
@@ -82,7 +82,7 @@ TEST_F(Engine_api_tests, SetEngineAttrNotSupported)
 
 TEST_F(Engine_api_tests, SetEngineAttrAlreadyFinalized)
 {
-    int64_t gidx = 0;
+    int64_t gidx = HIPDNN_TEST_PLUGIN_ID;
 
     test_util::populate_test_engine(_engine, &_graph, _handle, gidx, true);
     EXPECT_EQ(hipdnnBackendSetAttribute(
@@ -92,7 +92,7 @@ TEST_F(Engine_api_tests, SetEngineAttrAlreadyFinalized)
 
 TEST_F(Engine_api_tests, FinalizeEngine)
 {
-    int64_t gidx = 0;
+    int64_t gidx = HIPDNN_TEST_PLUGIN_ID;
 
     EXPECT_EQ(hipdnnBackendFinalize(_engine), HIPDNN_STATUS_BAD_PARAM);
     test_util::populate_test_engine(_engine, &_graph, _handle, gidx);
@@ -102,7 +102,7 @@ TEST_F(Engine_api_tests, FinalizeEngine)
 TEST_F(Engine_api_tests, GetEngineGraph)
 {
     hipdnnBackendDescriptor_t graph = nullptr;
-    int64_t gidx = 0;
+    int64_t gidx = HIPDNN_TEST_PLUGIN_ID;
 
     test_util::populate_test_engine(_engine, &_graph, _handle, gidx, true);
     EXPECT_EQ(hipdnnBackendGetAttribute(_engine,
@@ -119,7 +119,7 @@ TEST_F(Engine_api_tests, GetEngineGraph)
 
 TEST_F(Engine_api_tests, GetEngineGlobalIndex)
 {
-    int64_t gidx = 1;
+    int64_t gidx = HIPDNN_TEST_PLUGIN_ID;
     int64_t gidx_out;
 
     test_util::populate_test_engine(_engine, &_graph, _handle, gidx, true);
