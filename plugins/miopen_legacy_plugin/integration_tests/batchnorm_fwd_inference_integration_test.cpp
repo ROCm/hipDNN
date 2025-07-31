7f077791b1fe12082373c98b489f7684478492ad
Adam Dickin <adam.dickin@amd.com>
2025-07-31 21:27:29 +0000

    fix

--- plugins/miopen_legacy_plugin/integration_tests/batchnorm_fwd_inference_integration_test.cpp
+++ plugins/miopen_legacy_plugin/integration_tests/batchnorm_fwd_inference_integration_test.cpp
@@ -1,7 +1,6 @@
 // Copyright © Advanced Micro Devices, Inc., or its affiliates.
 // SPDX-License-Identifier:  MIT
 
-//#include <cmath>
 #include <cmath>
 #include <gtest/gtest.h>
 #include <hip/hip_runtime.h>
