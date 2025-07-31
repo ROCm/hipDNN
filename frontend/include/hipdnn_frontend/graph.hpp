2c773e4917ad7ceb7382ecbf9bfe1de6cf2d4249
Adam Dickin <adam.dickin@amd.com>
2025-07-31 21:26:11 +0000

    fixes

--- frontend/include/hipdnn_frontend/graph.hpp
+++ frontend/include/hipdnn_frontend/graph.hpp
@@ -175,8 +175,6 @@ public:
             }
         }
 
-        //TODO, SLAM the data types into the Y_tensor.
-
         std::vector<::flatbuffers::Offset<hipdnn_sdk::data_objects::Node>> nodes;
         for(auto& node : _sub_nodes)
         {
