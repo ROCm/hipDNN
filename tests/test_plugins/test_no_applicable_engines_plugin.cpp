d9116419b6b9ebb7db7499399bfea2f06c06c57b
Brian Harrison <brian.harrison@amd.com>
2025-08-08 19:31:03 +0000

    Merge testing plugins into a better reusable form

--- tests/test_plugins/test_no_applicable_engines_plugin.cpp
+++ tests/test_plugins/test_no_applicable_engines_plugin.cpp
@@ -1,291 +1,42 @@
 // Copyright © Advanced Micro Devices, Inc., or its affiliates.
 // SPDX-License-Identifier: MIT
 
-#include <iostream>
-
 #include "test_plugin_common.hpp"
-#include <hipdnn_sdk/data_objects/engine_details_generated.h>
-#include <hipdnn_sdk/logging/logger.hpp>
-#include <hipdnn_sdk/plugin/engine_plugin_api.h>
-#include <hipdnn_sdk/plugin/flatbuffer_utilities/engine_config_wrapper.hpp>
-#include <hipdnn_sdk/plugin/flatbuffer_utilities/graph_wrapper.hpp>
-#include <hipdnn_sdk/plugin/plugin_api.h>
-#include <hipdnn_sdk/plugin/plugin_data_type_helpers.hpp>
-#include <hipdnn_sdk/plugin/plugin_helpers.hpp>
-#include <hipdnn_sdk/plugin/plugin_last_error_manager.hpp>
-
-static const char* plugin_name = "test_no_applicable_engines_plugin";
-static const char* plugin_version = "1.0.0";
-
-enum
-{
-    TEST_ENGINE_ID = -3
-};
-
-using namespace hipdnn_plugin;
 
 // NOLINTNEXTLINE(modernize-avoid-c-arrays)
-thread_local char Plugin_last_error_manager::last_error[HIPDNN_PLUGIN_ERROR_STRING_MAX_LENGTH] = "";
+thread_local char
+    hipdnn_plugin::Plugin_last_error_manager::last_error[HIPDNN_PLUGIN_ERROR_STRING_MAX_LENGTH]
+    = "";
 
-extern "C" {
-
-hipdnnPluginStatus_t hipdnnPluginGetName(const char** name)
+class No_applicable_engines_plugin : public Test_plugin_base
 {
-    LOG_API_ENTRY("name_ptr={:p}", static_cast<void*>(name));
+public:
+    const char* get_plugin_name() const override
+    {
+        return "test_no_applicable_engines_plugin";
+    }
+    const char* get_plugin_version() const override
+    {
+        return "1.0.0";
+    }
+    int64_t get_engine_id() const override
+    {
+        return -3;
+    }
+    uint32_t get_num_applicable_engines() const override
+    {
+        return 0;
+    }
 
-    return try_catch([&, api_name = __func__]() {
-        throw_if_null(name);
+    // Since no engines are applicable, supports_engine_operations returns false
+    // This will cause all engine operations to throw appropriate errors
+};
 
-        *name = plugin_name;
-
-        LOG_API_SUCCESS(api_name, "plugin_name={:p}", static_cast<void*>(name));
-    });
+// Initialize plugin instance on load
+__attribute__((constructor)) static void initialize_plugin()
+{
+    Test_plugin_base::set_instance(std::make_unique<No_applicable_engines_plugin>());
 }
 
-hipdnnPluginStatus_t hipdnnPluginGetVersion(const char** version)
-{
-    LOG_API_ENTRY("version_ptr={:p}", static_cast<void*>(version));
-
-    return try_catch([&, api_name = __func__]() {
-        throw_if_null(version);
-
-        *version = plugin_version;
-
-        LOG_API_SUCCESS(api_name, "version={:p}", static_cast<void*>(version));
-    });
-}
-
-hipdnnPluginStatus_t hipdnnPluginGetType(hipdnnPluginType_t* type)
-{
-    LOG_API_ENTRY("type_ptr={:p}", static_cast<void*>(type));
-
-    return try_catch([&, api_name = __func__]() {
-        throw_if_null(type);
-
-        *type = HIPDNN_PLUGIN_TYPE_ENGINE;
-
-        LOG_API_SUCCESS(api_name, "type={}", *type);
-    });
-}
-
-void hipdnnPluginGetLastErrorString(const char** error_str)
-{
-    LOG_API_ENTRY("error_str_ptr={:p}", static_cast<void*>(error_str));
-
-    try_catch([&, api_name = __func__]() {
-        throw_if_null(error_str);
-
-        *error_str = Plugin_last_error_manager::get_last_error();
-
-        LOG_API_SUCCESS(api_name, "error_str={:p}", static_cast<void*>(error_str));
-    });
-}
-
-// Once plugins are loaded via plugin manager then logging will work for them
-hipdnnPluginStatus_t hipdnnPluginSetLoggingCallback(hipdnnCallback_t callback)
-{
-    return try_catch([&, api_name = __func__]() {
-        throw_if_null(callback);
-        hipdnn::logging::initialize_callback_logging(plugin_name, callback);
-        LOG_API_SUCCESS(api_name, "");
-    });
-}
-
-hipdnnPluginStatus_t hipdnnEnginePluginCreate(hipdnnEnginePluginHandle_t* handle)
-{
-    LOG_API_ENTRY("handle_ptr={:p}", static_cast<void*>(handle));
-
-    return try_catch([&, api_name = __func__]() {
-        throw_if_null(handle);
-
-        *handle = new hipdnnEnginePluginHandle();
-
-        LOG_API_SUCCESS(api_name, "created_handle={:p}", static_cast<void*>(*handle));
-    });
-}
-
-hipdnnPluginStatus_t hipdnnEnginePluginDestroy(hipdnnEnginePluginHandle_t handle)
-{
-    LOG_API_ENTRY("handle={:p}", static_cast<void*>(handle));
-
-    return try_catch([&, api_name = __func__]() {
-        throw_if_null(handle);
-
-        delete handle;
-        handle = nullptr;
-
-        LOG_API_SUCCESS(api_name, "");
-    });
-}
-
-hipdnnPluginStatus_t hipdnnEnginePluginSetStream(hipdnnEnginePluginHandle_t handle,
-                                                 hipStream_t stream)
-{
-    LOG_API_ENTRY(
-        "handle={:p}, stream_id={:p}", static_cast<void*>(handle), static_cast<void*>(stream));
-
-    return try_catch([&, api_name = __func__]() {
-        throw_if_null(handle);
-
-        LOG_API_SUCCESS(api_name, "");
-    });
-}
-
-hipdnnPluginStatus_t
-    hipdnnEnginePluginGetApplicableEngineIds(hipdnnEnginePluginHandle_t handle,
-                                             const hipdnnPluginConstData_t* op_graph,
-                                             int64_t* engine_ids,
-                                             uint32_t max_engines,
-                                             uint32_t* num_engines)
-{
-    LOG_API_ENTRY("handle={:p}, op_graph={:p}, engine_ids={:p}, max_engines={}, num_engines={:p}",
-                  static_cast<void*>(handle),
-                  static_cast<const void*>(op_graph),
-                  static_cast<void*>(engine_ids),
-                  max_engines,
-                  static_cast<void*>(num_engines));
-
-    return try_catch([&, api_name = __func__]() {
-        throw_if_null(handle);
-        throw_if_null(op_graph);
-        throw_if_null(engine_ids);
-        throw_if_null(num_engines);
-
-        // Return 0 engines - no applicable engines for this graph
-        *num_engines = 0;
-
-        LOG_API_SUCCESS(api_name, "num_engines={}", *num_engines);
-    });
-}
-
-hipdnnPluginStatus_t hipdnnEnginePluginGetEngineDetails(hipdnnEnginePluginHandle_t handle,
-                                                        int64_t engine_id,
-                                                        const hipdnnPluginConstData_t* op_graph,
-                                                        hipdnnPluginConstData_t* engine_details)
-{
-    LOG_API_ENTRY("handle={:p}, engine_id={}, op_graph={:p}, engine_details={:p}",
-                  static_cast<void*>(handle),
-                  engine_id,
-                  static_cast<const void*>(op_graph),
-                  static_cast<void*>(engine_details));
-
-    return try_catch([&]() {
-        throw_if_null(handle);
-        throw_if_null(op_graph);
-        throw_if_null(engine_details);
-
-        // Since this plugin reports no applicable engines, this function should fail
-        throw Hipdnn_plugin_exception(HIPDNN_PLUGIN_STATUS_INTERNAL_ERROR,
-                                      "No engines available - cannot get engine details");
-    });
-}
-
-hipdnnPluginStatus_t hipdnnEnginePluginDestroyEngineDetails(hipdnnEnginePluginHandle_t handle,
-                                                            hipdnnPluginConstData_t* engine_details)
-{
-    LOG_API_ENTRY("handle={:p}, engine_details={}",
-                  static_cast<void*>(handle),
-                  static_cast<void*>(engine_details));
-
-    return try_catch([&]() {
-        throw_if_null(handle);
-        throw_if_null(engine_details);
-
-        // Since we never create engine details, this should fail
-        throw Hipdnn_plugin_exception(HIPDNN_PLUGIN_STATUS_INTERNAL_ERROR,
-                                      "No engine details to destroy");
-    });
-}
-
-hipdnnPluginStatus_t
-    hipdnnEnginePluginGetWorkspaceSize(hipdnnEnginePluginHandle_t handle,
-                                       const hipdnnPluginConstData_t* engine_config,
-                                       const hipdnnPluginConstData_t* op_graph,
-                                       size_t* workspace_size)
-{
-    LOG_API_ENTRY("handle={:p}, engine_config={:p}, op_graph={:p}, workspace_size={:p}",
-                  static_cast<void*>(handle),
-                  static_cast<const void*>(engine_config),
-                  static_cast<const void*>(op_graph),
-                  static_cast<void*>(workspace_size));
-
-    return try_catch([&]() {
-        throw_if_null(handle);
-        throw_if_null(engine_config);
-        throw_if_null(op_graph);
-        throw_if_null(workspace_size);
-
-        // Since this plugin reports no applicable engines, this function should fail
-        throw Hipdnn_plugin_exception(HIPDNN_PLUGIN_STATUS_INTERNAL_ERROR,
-                                      "No engines available - cannot get workspace size");
-    });
-}
-
-hipdnnPluginStatus_t hipdnnEnginePluginCreateExecutionContext(
-    hipdnnEnginePluginHandle_t handle,
-    const hipdnnPluginConstData_t* engine_config,
-    const hipdnnPluginConstData_t* op_graph,
-    hipdnnEnginePluginExecutionContext_t* execution_context)
-{
-    LOG_API_ENTRY("handle={:p}, engine_config={:p}, op_graph={:p}, execution_context={:p}",
-                  static_cast<void*>(handle),
-                  static_cast<const void*>(engine_config),
-                  static_cast<const void*>(op_graph),
-                  static_cast<void*>(execution_context));
-
-    return try_catch([&]() {
-        throw_if_null(handle);
-        throw_if_null(engine_config);
-        throw_if_null(op_graph);
-        throw_if_null(execution_context);
-
-        // Since this plugin reports no applicable engines, this function should fail
-        throw Hipdnn_plugin_exception(HIPDNN_PLUGIN_STATUS_INTERNAL_ERROR,
-                                      "No engines available - cannot create execution context");
-    });
-}
-
-hipdnnPluginStatus_t hipdnnEnginePluginDestroyExecutionContext(
-    hipdnnEnginePluginHandle_t handle, hipdnnEnginePluginExecutionContext_t execution_context)
-{
-    LOG_API_ENTRY("handle={:p}, execution_context={:p}",
-                  static_cast<void*>(handle),
-                  static_cast<void*>(execution_context));
-
-    return try_catch([&]() {
-        throw_if_null(handle);
-        throw_if_null(execution_context);
-
-        // Since we never create execution contexts, this should fail
-        throw Hipdnn_plugin_exception(HIPDNN_PLUGIN_STATUS_INTERNAL_ERROR,
-                                      "No execution context to destroy");
-    });
-}
-
-hipdnnPluginStatus_t
-    hipdnnEnginePluginExecuteOpGraph(hipdnnEnginePluginHandle_t handle,
-                                     hipdnnEnginePluginExecutionContext_t execution_context,
-                                     void* workspace,
-                                     const hipdnnPluginDeviceBuffer_t* device_buffers,
-                                     uint32_t num_device_buffers)
-{
-    LOG_API_ENTRY("handle={:p}, execution_context={:p}, workspace={:p}, device_buffers={:p}, "
-                  "num_device_buffers={}",
-                  static_cast<void*>(handle),
-                  static_cast<void*>(execution_context),
-                  workspace,
-                  static_cast<const void*>(device_buffers),
-                  num_device_buffers);
-
-    return try_catch([&]() {
-        throw_if_null(handle);
-        throw_if_null(execution_context);
-        throw_if_null(device_buffers);
-
-        // Since this plugin reports no applicable engines, execution should fail
-        throw Hipdnn_plugin_exception(HIPDNN_PLUGIN_STATUS_INTERNAL_ERROR,
-                                      "No engines available - cannot execute graph");
-    });
-}
-
-} // extern "C"
+// Register all API functions
+REGISTER_TEST_PLUGIN_API()
