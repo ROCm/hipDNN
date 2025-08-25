ae39734731ea3b5a33b41674194a63833c6fd059
Brian Harrison <brian.harrison@amd.com>
2025-08-25 23:14:14 +0000

    Add explicit casting path just to ensure it's working as expected

--- sdk/include/hipdnn_sdk/test_utilities/cpu_fp_reference_miopen_rms_validation.hpp
+++ sdk/include/hipdnn_sdk/test_utilities/cpu_fp_reference_miopen_rms_validation.hpp
@@ -25,19 +25,20 @@ using namespace hipdnn_sdk::utilities;
 // RMS check is only relative tolerance based. We recommend using cpu_fp_reference_validation
 // instead, but this class can be used to compare with MIOpen tolerance checks.
 template <class T>
-class cpu_fp_reference_miopen_rms_validation : public Reference_validation_interface<T>
+class Cpu_fp_reference_miopen_rms_validation : public Reference_validation_interface<T>
 {
 public:
-    cpu_fp_reference_miopen_rms_validation(T relative_tolerance = std::numeric_limits<T>::epsilon())
-        : _relative_tolerance(relative_tolerance)
+    Cpu_fp_reference_miopen_rms_validation(T relative_tolerance = std::numeric_limits<T>::epsilon())
+        // Add explicit conversion path to ensure half & bfloat16 are converting without implicit cast.
+        : _relative_tolerance(static_cast<double>(static_cast<float>(relative_tolerance)))
     {
-        if(relative_tolerance < static_cast<T>(0.0f))
+        if(relative_tolerance < T{0})
         {
             throw std::invalid_argument("Tolerances must be non-negative");
         }
     }
 
-    ~cpu_fp_reference_miopen_rms_validation() override = default;
+    ~Cpu_fp_reference_miopen_rms_validation() override = default;
 
     bool all_close(Migratable_memory_interface<T>& reference,
                  Migratable_memory_interface<T>& implementation) override
@@ -64,8 +65,9 @@ public:
         // Iterate through all elements to calculate square differences and find max magnitudes
         for(size_t i = 0; i < element_count; ++i)
         {
-            auto ref_value = static_cast<double>(ref_data[i]);
-            auto impl_value = static_cast<double>(impl_data[i]);
+            // Add explicit conversion path to ensure half & bfloat16 are converting without implicit cast.
+            auto ref_value = static_cast<double>(static_cast<float>(ref_data[i]));
+            auto impl_value = static_cast<double>(static_cast<float>(impl_data[i]));
             
             // Accumulate square differences
             auto diff = ref_value - impl_value;
@@ -79,13 +81,14 @@ public:
         // Find the maximum magnitude between reference and implementation
         double max_magnitude = std::max({max_ref_magnitude, max_impl_magnitude, std::numeric_limits<double>::min()});
 
-        // Calculate and compare to _relative_tolerance
-        return (std::sqrt(square_difference) / (std::sqrt(static_cast<double>(element_count)) * max_magnitude)) < static_cast<double>(_relative_tolerance);
+        double relative_rms_error = std::sqrt(square_difference) / (std::sqrt(static_cast<double>(element_count)) * max_magnitude);
+        
+        return relative_rms_error <= _relative_tolerance;
     }
 
 private:
     // Tolerance for comparison
-    T _relative_tolerance;
+    double _relative_tolerance;
 };
 
 } // namespace reference_test_utilities
