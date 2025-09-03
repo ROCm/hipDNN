49656ff27baa029e7533642ad8e0a439b85b8dc1
Samuel Reeder <41528605+SamuelReeder@users.noreply.github.com>
2025-09-03 09:21:49 -0600

    Cleanup frontend POC graph and node attribute access patterns (#173)
    * Cleanup frontend POC graph and node attribute access patterns
    
    * Pointwise node use getters
    
    * Uncomment
    
    [ROCm/hipDNN commit: a7a5ab562c7b9d4cf6034b971f121ca69c25ecf1]

--- frontend/include/hipdnn_frontend/node/BatchnormNode.hpp
+++ frontend/include/hipdnn_frontend/node/BatchnormNode.hpp
@@ -164,7 +164,7 @@ public:
     {
         return hipdnn_sdk::data_objects::CreateNodeDirect(
             builder,
-            attributes.name.c_str(),
+            attributes.get_name().c_str(),
             hipdnn_sdk::data_objects::NodeAttributes::NodeAttributes_BatchnormAttributes,
             attributes.pack_attributes(builder).Union());
     }
