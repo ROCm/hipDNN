26f72aae5418c9a679811d4a3c858bb9326f685c
mousdahl-amd <mitch.ousdahl@amd.com>
2025-09-04 18:10:49 +0000

    Final cleanup on half / bfloat16 utilities

--- sdk/include/hipdnn_sdk/utilities/HalfUtils.hpp
+++ sdk/include/hipdnn_sdk/utilities/HalfUtils.hpp
@@ -7,21 +7,54 @@
 #include <hipdnn_sdk/logging/Logger.hpp>
 #include <string>
 
+inline __HOST_DEVICE__ half __ushort_as_half(unsigned short x)
+{
+    __half_raw r;
+    r.x = x;
+    return r;
+}
+
 inline __HOST_DEVICE__ half operator""_h(long double value)
 {
     return {static_cast<float>(value)};
+}
+
+inline __HOST_DEVICE__ half __habs(__half num)
+{
+    auto raw = static_cast<__half_raw>(num);
+    raw.x &= 0x7FFFu;
+    return raw;
+}
+
+inline __HOST_DEVICE__ bool __hisnan(__half x)
+{
+    __half_raw hr = x;
+    return (hr.x & 0x7FFFU) > 0x7C00u;
+}
+
+inline __HOST_DEVICE__ __half __hmax(const __half a, const __half b)
+{
+    if(__hisnan(a) && !__hisnan(b))
+        return b;
+    if(!__hisnan(a) && __hisnan(b))
+        return a;
+    if(__hisnan(a) && __hisnan(b))
+        return HIPRT_NAN_FP16;
+    if(static_cast<__half_raw>(a).x > static_cast<__half_raw>(b).x)
+        return __half_raw{static_cast<__half_raw>(a).x};
+    return __half_raw{static_cast<__half_raw>(b).x};
 }
 
 namespace std
 {
 inline __HOST_DEVICE__ half fabs(half num)
 {
-    return num > 0.0_h ? num : static_cast<half>(num * -1.0_h);
+    return __habs(num);
 }
 
 inline __HOST_DEVICE__ half max(half a, half b)
 {
-    return a > b ? a : b;
+    return __hmax(a, b);
 }
 
 }
--- sdk/include/hipdnn_sdk/utilities/HipBfloat16Utils.hpp
+++ sdk/include/hipdnn_sdk/utilities/HipBfloat16Utils.hpp
@@ -7,21 +7,57 @@
 #include <hipdnn_sdk/logging/Logger.hpp>
 #include <string>
 
+#define HIPRT_NAN_BF16 __ushort_as_bfloat16(static_cast<unsigned short>(0x7FFFU))
+
+inline __HOST_DEVICE__ hip_bfloat16 __ushort_as_bfloat16(const unsigned short int a)
+{
+    hip_bfloat16 val;
+    val.data = a;
+    return val;
+}
+
 inline __HOST_DEVICE__ hip_bfloat16 operator""_bf(long double value)
 {
     return hip_bfloat16(static_cast<float>(value));
+}
+
+inline __HOST_DEVICE__ hip_bfloat16 __habs(const hip_bfloat16 a)
+{
+    hip_bfloat16 abs = a;
+    abs.data &= 0x7FFF;
+    return abs;
+}
+
+inline __HOST_DEVICE__ bool __hisnan(const hip_bfloat16 a)
+{
+    hip_bfloat16 hr = a;
+    return !(~hr.data & 0x7f80) && +(hr.data & 0x7f);
+}
+
+//#define HIPRT_NAN_BF16 __ushort_as_bfloat16((unsigned short)0x7FFFU)
+
+inline __HOST_DEVICE__ hip_bfloat16 __hmax(const hip_bfloat16 a, const hip_bfloat16 b)
+{
+    auto a_nan = __hisnan(a), b_nan = __hisnan(b);
+    if(a_nan || b_nan)
+    {
+        if(a_nan && b_nan)
+            return HIPRT_NAN_BF16; // return canonical NaN
+        return a_nan ? b : a;
+    }
+    return a.data > b.data ? a : b;
 }
 
 namespace std
 {
 inline __HOST_DEVICE__ hip_bfloat16 fabs(hip_bfloat16 num)
 {
-    return num > 0.0_bf ? num : num * -1.0_bf;
+    return __habs(num);
 }
 
 inline __HOST_DEVICE__ hip_bfloat16 max(hip_bfloat16 a, hip_bfloat16 b)
 {
-    return a > b ? a : b;
+    return __hmax(a, b);
 }
 }
 
