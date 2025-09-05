f9c66e5b2b258930062bfe3bf935b1dbb323d06d
Adam Dickin <adam.dickin@amd.com>
2025-09-05 16:29:01 +0000

    commit initial tests.cmake

--- cmake/Tests.cmake
+++ cmake/Tests.cmake
@@ -94,11 +94,15 @@ function(_add_gtest_target_internal APPEND_FUNCTION_SUFFIX TARGET WORKING_DIR)
         message(FATAL_ERROR "Unknown test type suffix: ${APPEND_FUNCTION_SUFFIX}")
     endif()
     
+    set_target_properties(${TARGET} PROPERTIES
+        RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
+    )
+
     add_dependencies(check_ctest ${TARGET})
-    gtest_discover_tests(
-        ${TARGET}
+    add_test(
+        NAME ${TARGET}
+        COMMAND ${TARGET} 
         WORKING_DIRECTORY ${WORKING_DIR}
-        DISCOVERY_MODE PRE_TEST
     )
 endfunction()
 
