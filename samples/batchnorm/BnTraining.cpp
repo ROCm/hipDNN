4efc6d59c8f49311524a2f11c6f8d20f57387a9b
Samuel Reeder <samuel.reeder@amd.com>
2025-09-08 18:25:02 +0000

    Modify samples and frontend tests

--- samples/batchnorm/BnTraining.cpp
+++ samples/batchnorm/BnTraining.cpp
@@ -189,7 +189,7 @@ int main(int argc, char* argv[])
 {
     auto config = parseCommandLineArgs(argc, argv);
 
-    initializeFrontendLogging(hipdnnLoggingCallback_ext);
+    initializeFrontendLogging();
 
     hipdnnHandle_t handle;
     HIPDNN_CHECK(hipdnnCreate(&handle));
