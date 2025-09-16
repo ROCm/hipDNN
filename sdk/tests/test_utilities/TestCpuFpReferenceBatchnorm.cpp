91b472e1d294d152f9e17e3ca3ff11922565794e
Brian Harrison <brian.harrison@amd.com>
2025-09-16 21:17:46 +0000

    Fix incorrect stride ordering for 5D dims leading to ASAN issues

--- sdk/tests/test_utilities/TestCpuFpReferenceBatchnorm.cpp
+++ sdk/tests/test_utilities/TestCpuFpReferenceBatchnorm.cpp
@@ -658,9 +658,11 @@ TEST(TestCpuFpReferenceBatchnormFp32, BatchnormFwdTraining3D)
 
 TEST(TestCpuFpReferenceBatchnormFp32, BatchnormFwdTraining5D)
 {
+    TensorLayout ncdhw{.name = "NCDHW", .strideOrder = {4, 3, 2, 1, 0}};
+
     // Test with 5D tensor (batch, channel, depth, height, width)
-    Tensor<float> inputTensor({2, 3, 4, 5, 6});
-    Tensor<float> outputTensor({2, 3, 4, 5, 6});
+    Tensor<float> inputTensor({2, 3, 4, 5, 6}, ncdhw);
+    Tensor<float> outputTensor({2, 3, 4, 5, 6}, ncdhw);
     Tensor<float> scaleTensor({1, 3});
     Tensor<float> biasTensor({1, 3});
     Tensor<float> savedMean({1, 3});
