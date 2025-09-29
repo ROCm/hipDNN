d1fd3253caa2bed6ba54313bc024374d4c4e36f3
BrianHarrisonAMD <169072757+BrianHarrisonAMD@users.noreply.github.com>
2025-09-29 14:09:45 +0000

    [rocm-libraries] ROCm/rocm-libraries#1857 (commit 55e3eb3)
    [hipDNN] Add option to disable clang-format
    
    ## Motivation
    
    For adding hipDNN to TheRock clang-format isn't available during
    configure stage, and causes issues.
    Clang-format and other checks will happen separately during presubmit,
    and we need the option to disable them to prevent errors during
    configure.

--- cmake/ClangCheck.cmake
+++ cmake/ClangCheck.cmake
@@ -1,23 +1,25 @@
 # Copyright © Advanced Micro Devices, Inc., or its affiliates.
 # SPDX-License-Identifier:  MIT
 
-include(${CMAKE_CURRENT_LIST_DIR}/CheckToolVersion.cmake)
+if(ENABLE_CLANG_FORMAT)
+    include(${CMAKE_CURRENT_LIST_DIR}/CheckToolVersion.cmake)
 
-set(CLANG_FORMAT_PRUNE -path "./build" -prune -o -path "./sdk/include/hipdnn_sdk/data_objects" -prune -o)
+    set(CLANG_FORMAT_PRUNE -path "./build" -prune -o -path "./sdk/include/hipdnn_sdk/data_objects" -prune -o)
 
-# Find and check clang-format version using unified function
-findAndCheckClangFormat()
+    # Find and check clang-format version using unified function
+    findAndCheckClangFormat()
 
-add_custom_target(
-    check_format
-    COMMAND  find . ${CLANG_FORMAT_PRUNE} -regex ".*\\.\\(cpp\\|hpp\\|c\\|h\\)" -exec ${CLANG_FORMAT_BINARY} --dry-run --Werror {} +
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    VERBATIM
-)
+    add_custom_target(
+        check_format
+        COMMAND  find . ${CLANG_FORMAT_PRUNE} -regex ".*\\.\\(cpp\\|hpp\\|c\\|h\\)" -exec ${CLANG_FORMAT_BINARY} --dry-run --Werror {} +
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        VERBATIM
+    )
 
-add_custom_target(
-    format
-    COMMAND  find . ${CLANG_FORMAT_PRUNE} -regex ".*\\.\\(cpp\\|hpp\\|c\\|h\\)" -exec ${CLANG_FORMAT_BINARY} --verbose -i {} +
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    VERBATIM
-)
+    add_custom_target(
+        format
+        COMMAND  find . ${CLANG_FORMAT_PRUNE} -regex ".*\\.\\(cpp\\|hpp\\|c\\|h\\)" -exec ${CLANG_FORMAT_BINARY} --verbose -i {} +
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        VERBATIM
+    )
+endif()
